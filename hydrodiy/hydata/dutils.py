72bb7ac58b3497c1886d280c1ed77ddb1fe0ec11
Julien Lerat <julien.lerat@gmail.com>
2014-08-07 21:37:04 +1000

    Added function to compute cum clim (test not finalised)

--- hydrodiy/hydata/dutils.py
+++ hydrodiy/hydata/dutils.py
@@ -2,6 +2,8 @@ import datetime
 import numpy as np
 import pandas as pd
 
+from hystat import sutils
+
 def wyear(dt, start_month=7):
     ''' compute water year of a particular day '''
     return dt.year - int(dt.month<start_month)
@@ -51,6 +53,50 @@ def runclim(data, nwin=10):
         else:
             clim = clim.append(df)
 
+    clim = clim.rename(columns={'50%':'median'})
+
     return clim
+
+def runclimcum(data, clim=None, nwin=10):
+    ''' Gapfill time series '''
+
+    # Get climatology
+    if clim is None:
+        clim = runclim(data, nwin)
+
+    # Find beginning of water year
+    ts = pd.Series(clim['median'].values, 
+            index = pd.date_range('2000-01-01', freq='D', periods=366))
+    climm = ts.resample('MS') 
+    start = (climm.index[climm==np.max(climm)].dayofyear-183)%366
+
+    # reorganise series
+    ts = pd.DataFrame({'data':data.values, 
+            'dayfromstart':1+(data.index.dayofyear-start)%366})    
+
+    ts['year'] = ts['dayfromstart']==1
+    ts['year'] = ts['year'].astype('int').cumsum()
+    tsc = pd.pivot_table(ts, rows='dayfromstart', 
+            cols='year', values='data')
+
+    # Gapfill
+    for i in tsc.index:
+        val = clim['median'][(i-1+start)%366].values[0]
+        ccx = pd.isnull(tsc.loc[i,:])
+        tsc.loc[i,ccx] = val
+
+    # compute cum sum
+    tsc = tsc.cumsum(axis=0)
+
+    # Compute stats
+    out =  tsc.apply(lambda x: x.describe(), axis=1)
+    out = out.rename(columns={'50%':'median'})
+
+    # Find lowest/highest year
+    total = tsc.loc[366,:]
+    out['lowest'] = tsc.loc[:, total==np.min(total)]
+    out['highest'] = tsc.loc[:, total==np.max(total)]
+
+    return out
 
 
--- hydrodiy/hydata/tests/test_hydata_dutils.py
+++ hydrodiy/hydata/tests/test_hydata_dutils.py
@@ -41,14 +41,28 @@ class UtilsTestCase(unittest.TestCase):
         self.assertEqual(dutils.cycledist(1, 360, start=1, end=365), 6)
 
     def test_runclim(self):
-        index = pd.date_range('1960-01-01', '2000-12-31', freq='D')
+        index = pd.date_range('1900-01-01', '2020-12-31', freq='d')
         n = len(index)
         u = np.sin((0.+index.dayofyear)/366*2*np.pi)
         u += 0.2*np.random.uniform(size=n)-0.1
         s = pd.Series(u, index=index)
-        cl = dutils.runclim(s)
-        
+        clim = dutils.runclim(s)
+        v = np.sin((0.+np.arange(366))/366*2*np.pi)
+        err = np.abs(clim['median']-v)/(1+np.abs(v))
+        self.assertTrue(np.max(err)<0.03)
 
+    def test_runclimcum(self):
+        index = pd.date_range('1950-01-01', '2020-12-31', freq='D')
+        n = len(index)
+        u = np.sin((0.+index.dayofyear)/366*2*np.pi)
+        u += 0.5*np.random.uniform(size=n)-0.25
+        u = u*u
+        s = pd.Series(u, index=index)
+        climc = dutils.runclimcum(s)
+
+        #import matplotlib.pyplot as plt
+        #climc[['lowest', 'median', 'highest']].plot(); plt.show()
+        #TODO
 
 if __name__ == "__main__":
     unittest.main()
