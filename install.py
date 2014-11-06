202b99bc3a3101d3d6dbbab7134eda7ddf271600
Julien Lerat <julien.lerat@gmail.com>
2014-11-06 20:15:56 +1100

    Removed user in install to work with virtualenv

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
 import os
-os.system('python setup.py install --user --record package_files.txt')
+#os.system('python setup.py install --user --record package_files.txt')
+os.system('python setup.py install --record package_files.txt')
 os.system('python setup.py sdist --formats=gztar,zip')
