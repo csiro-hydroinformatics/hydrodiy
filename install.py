2cb752c3c06e8a3a079c1a9caa6cd8d6bf1a583e
Julien Lerat <julien.lerat@gmail.com>
2015-01-14 19:36:46 +1100

    Increased flexibility in install

--- install.py
+++ install.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-import os
-#os.system('python setup.py install --user --record package_files.txt')
-os.system('python setup.py install --record package_files.txt')
+import os, sys
+
+if len(sys.argv)>1:
+    option = sys.argv[1]
+    os.system('python setup.py install %s --record package_files.txt' % option)
+else:
+    os.system('python setup.py install --record package_files.txt')
+
 os.system('python setup.py sdist --formats=gztar,zip')
