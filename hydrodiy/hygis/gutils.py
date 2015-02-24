8ea3b4b2bb1ab7a952d436fc9ce802a82942830b
Julien Lerat <julien.lerat@gmail.com>
2015-02-24 23:41:10 +1100

    Added alpha shape algo - does not work

--- hydrodiy/hygis/gutils.py
+++ hydrodiy/hygis/gutils.py
@@ -2,6 +2,8 @@ import numpy as np
 import math
 import matplotlib.image as mpimg
 
+from hystat import sutils
+
 def points_inside_polygon(points, poly, rtol=1e-8, atol=1e-8):
     ''' 
     
@@ -76,46 +78,51 @@ def plot_geoimage(ax, imgfile):
     ax.imshow(img, extent=extent)
 
 
-def smoothpolygon(xy, tol=1e-2, maxremove=0.5):
-    ''' 
-    [FUNCTION IS NOT TESTED]
-    Smooth a polygon defined by x/y coordinates by removing spikes, i.e.
-    removing points lying far away from their neighbours.
+def alphashape(points, alphathresh = 10):
+    ''' Alpha shape concave hull algorithm inspired by
+        - http://sgillies.net/blog/1155/the-fading-shape-of-alpha/
+        - http://blog.thehumangeo.com/2014/05/12/drawing-boundaries-in-python/
 
-    :param numpy.array xy : A polygon defined by a 2d numpy array [x,y]
-    :param float tol : Tolerance for smooth factor. Values higher than 1e-2 allow more roughness
-    :param float maxremove : Maximum proportion of points that can be removed
+        CODE DOES NOT WORK!
 
+        :param numpy.array points : A list of points given as a 2d numpy array
+        :param float alphathresh: Threshold on alpha values expresed as percentile from radius distribution
     '''
 
-    # Characteristic dimension of polygon
-    min = np.min(xy, 0)
-    max = np.max(xy, 0)
-    w = math.sqrt(np.sum((max-min)**2))
+    tri = Delaunay(points)
+    edges = set()
+    edge_points = []
+    nv = len(tri.vertices)
 
-    # Initialise 
-    ipb = [True]*len(xy) 
-    n = len(xy)
-    nmin = int(float(n)*maxremove)
-    xy2 = xy.copy()
+    # Alpha-shape algorithm
+    circum_r = np.zeros(nv)
+    for i in range(len(tri.vertices)):
+        v = tri.vertices[i]
+        pa = points[v[0]]
+        pb = points[v[1]]
+        pc = points[v[2]]
+    
+        # Lengths of sides of triangle
+        a = math.sqrt((pa[0]-pb[0])**2 + (pa[1]-pb[1])**2)
+        b = math.sqrt((pb[0]-pc[0])**2 + (pb[1]-pc[1])**2)
+        c = math.sqrt((pc[0]-pa[0])**2 + (pc[1]-pa[1])**2)
+    
+        # Semiperimeter of triangle
+        s = (a + b + c)/2.0
+    
+        # Area of triangle by Heron's formula
+        area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    
+        circum_r[i] = a*b*c/(4.0*area)
 
-    # Remove spikes iteratively, until no spike remains
-    while (np.sum(ipb)>0) & (n>nmin):
-        n = len(xy2)
-        d2 = np.sum((xy2[2:]-xy2[:-2])**2, 1)
-        d1 = np.sum((xy2[1:]-xy2[:-1])**2, 1).reshape(n-1, 1)
-        d1m = np.min(np.concatenate((d1[1:], d1[:-1]),1), 1)
+    # Define the alpha threshold as 
+    alpha = sutils.percentiles(circum_r, pthresh).squeeze()
 
-        metric = (d2-d1m)/w
-        metric = np.insert(metric, [0, 1], 0)
-        ipb = metric > 1e-2
-        xy2 = xy2[~ipb]
+    # Here's the radius filter.
+    idx = np.where(circum_r < alpha)[0]
+    v = tri.vertices[idx]
 
-    return xy2
+    edge_points = np.array(edge_points)
 
-
-
-
-
-
+    return edge_points
 
