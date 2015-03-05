c5e0d1f653c94056ce9a2512652a9f0686b936fb
Julien Lerat <julien.lerat@gmail.com>
2015-03-05 19:29:34 +1100

    Replaced hygis.oz code with matplotlib.basemap

--- hydrodiy/hygis/oz.py
+++ hydrodiy/hygis/oz.py
@@ -1,113 +1,62 @@
-import os
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.path import Path
-import matplotlib.patches as patches
-
-from hygis import gutils
+import matplotlib.pyplot as plt
+from mpl_toolkits import basemap
 
 class Oz:
-    ''' retrieve Australia coast lines and state boundaries '''
+    ''' Plot Australia coast lines and state boundaries '''
 
-    def __init__(self):
-        FHYGIS, ozfile = os.path.split(__file__)
-        polygons_file = os.path.join(FHYGIS, 'data',
-                                    'oz_data.csv')
-        self.polygons_xy = pd.read_csv(polygons_file)
+    def __init__(self, ax=None, 
+        ulat=-9., llat=-43., llon=108., rlon=151.5,
+        resolution='l', 
+        remove_axis = True):
 
-        centroids_file = os.path.join(FHYGIS, 'data',
-                                    'oz_states_centroids.csv')
-        self.centroids_xy = pd.read_csv(centroids_file, 
-                                    index_col='state')
-        self.reliefmap_file = os.path.join(FHYGIS, 'data',
-                                    'australia.png')
-
-    def get_polygon_xy(self, item):
-        idx = self.polygons_xy.item==item
-        return self.polygons_xy.ix[idx]
-
-    def get_range(self, item=None):
-        ''' Get x/y range for a map item (e.g. QLD)'''
-        if item is None:
-            return (112., 154., -45., -8.)
+        self.ulat = ulat
+        self.llat = llat
+        self.llon = llon
+        self.rlon = rlon
+        if ax is None:
+            self.ax = plt.gca()
         else:
-            xy = self.get_polygon_xy(item)
-            mini = xy.min()
-            maxi = xy.max()
-            return (mini['x'], maxi['x'], 
-                    mini['y'], maxi['y'])
+            self.ax = ax
 
-    def set_rangeoz(self, ax, turn_off_axis=True):
+        self.map = basemap.Basemap(self.llon, self.llat, self.rlon, self.ulat, 
+            lat_0=24.75, lon_0=134.0, lat_1=-10, lat_2=-40, 
+            rsphere=(6378137.00,6356752.3142), 
+            projection='lcc', resolution=resolution,
+            area_thresh=1000, suppress_ticks=True, ax = self.ax)
+
+        if remove_axis:
+            self.ax.axis('off')
+
+    def get_range(self):
+        ''' Get x/y range for the map '''
+
+        return (self.llon, self.rlon, self.ulat, self.llat)
+
+    def set_axrange(self):
         ''' Get nice x/y range for Australian coastline '''
-        ax.axis('equal')
-        ax.set_xlim((112.0, 154.0))
-        ax.set_ylim((-45.0, -8.0))
-        if turn_off_axis:
-            ax.axis('off')
+        self.ax.set_xlim((self.llon, self.rlon))
+        self.ax.set_ylim((self.ulat, self.llat))
 
-    def plot_items(self, ax, items=['coast_mainland','TAS']):
-        ''' plot a list of items (e.g. QLD, TAS)'''
-        if type(items) is not list:
-            items = [items]
-        for it in items:
-            xy = self.get_polygon_xy(it)
-            ids = np.unique(xy.id)
-            for id in ids:
-                idx = xy.id==id
-                ax.plot(xy.x[idx], xy.y[idx], 'k-')
-
-    def plot_coast(self, ax):
+    def drawcoast(self):
         ''' plot coast line '''
-        self.plot_items(ax, ['TAS', 'coast_mainland'])
+        self.map.drawcoastlines()
 
-    def plot_ozpng(self, ax):
+    def drawrelief(self):
         ''' plot shaded relief map '''
-        gutils.plot_geoimage(ax, self.reliefmap_file)
+        self.map.shadedrelief()
 
-    def plot_states(self, ax):
+    def drawstates(self, linestyle='--'):
         ''' plot states boundaries '''
-        states = ['NSW', 'QLD', 'NT', 'VIC', 'WA', 'TAS', 'SA']
-        self.plot_items(ax, states)
+        self.map.drawstates(linestyle=linestyle)
 
-    def plot_states_data(self, ax, data, fontsize=10):
-        ''' plot data for each state in the plot margin 
-            data is a pandas.Series with state name
-            as index (NSW, ACT, ...)
-        '''
-        for state, value in data.iteritems():
-            xy = self.centroids_xy.loc[state,:]    
-            x1 = xy['xcenter']
-            y1 = xy['ycenter']
-            x2 = xy['xoutside']
-            y2 = xy['youtside']
-            ha = xy['ha']
-            va = xy['va']
-            ax.annotate(value, 
-                    arrowprops={'edgecolor':'grey',
-                        'facecolor':'grey', 'width':1,
-                        'headwidth':4}, size=fontsize,
-                    bbox={'facecolor':'white', 'edgecolor':'white'},
-                    va=va, ha=ha,
-                    xy=(x1, y1), xytext=(x2, y2))
+    def plot(self, long, lat, *args, **kwargs):
+        ''' Plot points in map '''
 
-    def plot_mask(self, ax, 
-            facecolor='white', 
-            alpha=1.0):
-        ''' plot the coastline mask '''
-        for m in ['mask_mainland','mask_tas']:
-            xy = self.get_polygon_xy(m)
-            nval = len(xy)
-            verts = zip(xy.x, xy.y)
-            codes = [Path.LINETO]*nval
-            codes[0] = Path.MOVETO
-            codes[-1] = Path.CLOSEPOLY
-            path = Path(verts, codes)
-            patch = patches.PathPatch(path, 
-                        facecolor=facecolor, 
-                        edgecolor=facecolor,
-                        alpha=alpha)
-            ax.add_patch(patch)
+        if len(long) != len(lat):
+            raise ValueError('len(long) (%d) != len(lat) (%d)' % (len(long), len(lat)))
 
+        x, y = self.map(long, lat)
+        self.map.plot(x, y, *args, **kwargs)
 
+             
 
--- hydrodiy/hygis/tests/test_hygis_oz.py
+++ hydrodiy/hygis/tests/test_hygis_oz.py
@@ -12,45 +12,46 @@ class OzTestCase(unittest.TestCase):
         FTEST, ozfile = os.path.split(__file__)
         self.FOUT = FTEST
 
-    def test_oz1(self):
+    def test_oz0(self):
+
+        plt.close('all')
+
         om = Oz()
-        plt.close()
+        om.drawcoast()
+        om.drawstates()
+      
+        fp = '%s/oz_plot0.png'%self.FOUT
+        plt.savefig(fp)
+
+    def test_oz1(self):
+
+        plt.close('all')
         fig, ax = plt.subplots()
-        npt = 10
+
+        om = Oz(ax)
+        om.drawcoast()
+        om.drawstates()
+
+        npt = 100
         x = np.random.normal(loc=133, scale=20, size=npt)
         y = np.random.normal(loc=-25, scale=20, size=npt)
-        ax.plot(x,y,'bo')
-        om.plot_states(ax)
-        om.set_rangeoz(ax)
+        om.plot(x, y, 'ro')
+       
         putils.footer(fig) 
         fp = '%s/oz_plot1.png'%self.FOUT
-        plt.savefig(fp)
+        fig.savefig(fp)
 
     def test_oz2(self):
+
+        plt.close('all')
+
         om = Oz()
-        plt.close()
-        fig, ax = plt.subplots()
-        npt = 10
-        om.plot_states(ax)
-        data = pd.Series(['%0.3f'%a for a in np.random.random(3)], 
-                index=['ACT', 'NT', 'SA'])
-        om.plot_states_data(ax, data)        
-        om.set_rangeoz(ax)
-        putils.footer(fig) 
+        om.drawrelief()
+        om.drawcoast()
+        om.drawstates()
+      
         fp = '%s/oz_plot2.png'%self.FOUT
         plt.savefig(fp)
-
-    def test_oz3(self):
-        om = Oz()
-        plt.close()
-        fig, ax = plt.subplots()
-        om.plot_ozpng(ax)
-        om.plot_states(ax)
-        om.set_rangeoz(ax)
-        putils.footer(fig) 
-        fp = '%s/oz_plot3.png'%self.FOUT
-        plt.savefig(fp)
-
 
 if __name__ == "__main__":
     unittest.main()
