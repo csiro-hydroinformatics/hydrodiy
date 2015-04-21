89462ee70261a9aaaeb8972f8feed72b2c57e6b6
Julien Lerat <julien.lerat@gmail.com>
2015-04-21 20:27:35 +1000

    Added list of sites + extended meta data to hykiwis

--- hydrodiy/hydata/tests/test_hydata_hykiwis.py
+++ hydrodiy/hydata/tests/test_hydata_hykiwis.py
@@ -12,22 +12,47 @@ class HyKiwisTestCase(unittest.TestCase):
         print('\t=> HyKiwisTestCase (hydata)')
         self.dt = None # TODO
 
+    def test_getsites(self):
+       
+        hyk = hykiwis.HyKiwis()
+
+        sites = hyk.get_sites()
+
+        #import pdb; pdb.set_trace()
+ 
     def test_getattrs(self):
        
         hyk = hykiwis.HyKiwis()
 
-        attrs = hyk.getattrs('410001')
-        self.assertEqual(attrs['tsid'], '54940042')
+        attrs = hyk.get_tsattrs('410001')
+        self.assertEqual(attrs['ts_id'], '54940042')
+        self.assertEqual(attrs['ts_unitsymbol'], 'Ml/d')
+        self.assertEqual(attrs['station_no'], '410001')
         
-        attrs = hyk.getattrs('613002')
-        self.assertEqual(attrs['tsid'], '158791042')
+        attrs = hyk.get_tsattrs('613002')
+        self.assertEqual(attrs['ts_id'], '158791042')
+        self.assertEqual(attrs['ts_unitsymbol'], 'Ml/d')
+        self.assertEqual(attrs['station_no'], '613002')
+
+    def test_getattrs_all(self):
+               
+        hyk = hykiwis.HyKiwis()
+
+        sites = hyk.get_sites()
+
+        idx = sites['station_no'].apply(lambda x: x.startswith('41073'))
+        sites = sites[idx]
+
+        attrs_all = hyk.get_tsattrs_all(sites)
+
+        self.assertEqual(attrs_all.shape, (6, 8))
  
     def test_getdata(self):
        
         hyk = hykiwis.HyKiwis()
 
-        attrs = hyk.getattrs('410001')
-        ts_data = hyk.getdata(attrs, '1980-01-01', '1980-01-05')
+        attrs = hyk.get_tsattrs('410001')
+        ts_data = hyk.get_data(attrs, '1980-01-01', '1980-01-05')
 
         self.assertEqual(list(ts_data.astype(int)), 
                 [13964, 14018, 15702, 16402, 15410])
