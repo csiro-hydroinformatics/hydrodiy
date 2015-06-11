d9c5ea1a6314e3aad9f6390545208a995806db32
Julien Lerat <julien.lerat@gmail.com>
2015-06-11 21:27:41 +1000

    Improved csv

--- hydrodiy/hyio/csv.py
+++ hydrodiy/hyio/csv.py
@@ -1,44 +1,98 @@
-import sys
-import os
-import re
-import time
-import string
+import sys, os, re, time, string
+
 import gzip
+
+has_distutils = False
+try:
+    from distutils.sysconfig import get_python_inc
+    from distutils.sysconfig import get_python_lib
+    has_distutils = True
+
+except ImportError:
+    pass
+
 import pandas as pd
+import numpy as np
+
+# Max length of comment keys
+key_length_max = 30
+
+
+def _header2comment(header):
+
+    comment = {}
+    i = 1
+
+    for s in header:
+
+        if not bool(re.search('-{10}', s)):
+
+            k = re.sub('\:.*$', '', s)
+            val = re.sub('%s:' % k, '', s).strip()
+            k = re.sub(' +', '_', k.strip().lower())
+
+            if not bool(re.search('\:', s[:key_length_max])):
+                k = 'comment_%2.2d' % i
+                val = s
+                i += 1
+
+            if val != '':
+                comment[k] = val
+
+    return comment
+
 
 def _csvhead(nrow,ncol,comment,src='uknown'):
     """ Produces a nice header for csv files """
+
     comment_list = comment
+    
     if not isinstance(comment, list):
         comment_list = [comment]
 
     h = []
     h.append("# --------------------------------------------------")
-    h.append("# File comments :")
-    h.append("# nrow = %d ncol= %d"%(nrow,ncol))
-    for co in comment_list: 
-        h.append("# %s"%co)
-    h.append("# ")
-    h.append("# File info :")
-    h.append("# Written on : %s"%time.strftime("%Y-%m-%d %H:%M"))
+    
+    h.append("# nrow : %d" % nrow)
+    h.append("# ncol : %d" % ncol)
+
+    for i in range(len(comment_list)): 
+        h.append("# comment%2.2d : %s" %(i, comment_list[i]))
+
+    h.append("# time_written : %s" % time.strftime("%Y-%m-%d %H:%M"))
+    
     try:
-        h.append("# Author : %s"%os.getlogin())
-    except (OSError, AttributeError):
-        h.append("# Author : ")
-    h.append("# Produced with python script : %s"%src)
-    h.append("# Python work dir : %s"%os.getcwd())
-    h.append("# Python environment : %s"%os.name)
-    h.append("# Python version : %s"%sys.version.replace("\n"," "))
+        h.append("# author : %s" % os.getlogin())
+    except:
+        h.append("# author : unknwon")
+
+    h.append("# script : %s" % src)
+    h.append("# work_dir : %s" % os.getcwd())
+    h.append("# python_environment : %s" % os.name)
+    h.append("# python_version : %s" % sys.version.replace("\n"," "))
+    h.append("# pandas_version : %s" % pd.__version__)
+    h.append("# numpy_version : %s" % np.__version__)
+
+    if has_distutils:
+        h.append("# python_inc : %s" % get_python_inc())
+        h.append("# python_lib : %s" % get_python_lib())
+    
+    h.append("# --------------------------------------------------")
 
     return h
 
-def write_csv(data, filename, comment, index=False, source='unknown', compress=True):
+
+def write_csv(data, filename, comment, source,
+        index=False,
+        compress=True, **kwargs):
     """ write a pandas dataframe to csv with comments """
     
-    head = _csvhead(data.shape[0], data.shape[1], comment, src=source)
+    head = _csvhead(data.shape[0], data.shape[1], 
+                comment, src=source)
    
     # defines file name
     filename_full = filename
+
     if compress and ~filename.endswith('.gz'):
         filename_full += '.gz'
     
@@ -51,13 +105,17 @@ def write_csv(data, filename, comment, index=False, source='unknown', compress=T
     for line in head:
         fcsv.write('%s\n'%line)
 
-    data.to_csv(fcsv, index=index)
+    data.to_csv(fcsv, index=index, **kwargs)
 
     fcsv.close()
 
 
-def read_csv(filename, has_header=True):
+def read_csv(filename, has_colnames=True, **kwargs):
     """ Reads data with comments on top to a pandas data frame"""
+
+    # Add gz if file does not exists
+    if not os.path.exists(filename):
+        filename = '%s.gz' % filename
 
     # Open proper file type
     if filename.endswith('gz'):
@@ -66,30 +124,40 @@ def read_csv(filename, has_header=True):
         fcsv = open(filename, 'r')
 
     # Reads content
-    comment = []
-    if has_header:
+    header = []
+    comment = {}
+
+    if has_colnames:
+
         line = fcsv.readline()
+
         while line.startswith('#'):
-            comment.append(re.sub('^# *|\n$', '', line))
+            header.append(re.sub('^# *|\n$', '', line))
             line = fcsv.readline()
+
+        comment = _header2comment(header)
         
         # deals with multi-index columns
         # reformat columns like (idx1-idx2-...)
         se = re.findall('\"\([^\(]*\)\"', line)
         linecols = line
+
         if se:
             for cn in se:
                 cn2 = re.sub('\(|\)|\"|\'','',cn)
                 cn2 = re.sub(', *','-',cn2)
                 linecols = re.sub(re.escape(cn), cn2, linecols)
+
         cns = string.strip(linecols).split(',') 
 
         # Reads data with proper column names    
-        data = pd.read_csv(fcsv, names=cns)
+        data = pd.read_csv(fcsv, names=cns, **kwargs)
+
         data.columns = [re.sub('\\.','_',cn) 
                         for cn in data.columns] 
+
     else:
-        data = pd.read_csv(fcsv, header=None)
+        data = pd.read_csv(fcsv, header=None, **kwargs)
 
     fcsv.close()
 
--- hydrodiy/hyio/tests/test_hyio_csv.py
+++ hydrodiy/hyio/tests/test_hyio_csv.py
@@ -11,23 +11,33 @@ class CsvTestCase(unittest.TestCase):
         FTEST, testfile = os.path.split(__file__)
         self.FOUT = FTEST
         
+
     def test_read_csv1(self):
+
         fcsv = '%s/states_centroids.csv.gz'%self.FOUT
+
         data, comment = csv.read_csv(fcsv)
-         
+
         st = pd.Series(['ACT', 'NSW', 'NT', 'QLD', 'SA', 
                             'TAS', 'VIC', 'WA'])
         self.assertTrue(all(data['state']==st))
         
+
     def test_read_csv2(self):
+
         fcsv = '%s/states_centroids_noheader.csv'%self.FOUT
-        data, comment = csv.read_csv(fcsv, has_header=False)
+
+        data, comment = csv.read_csv(fcsv, has_colnames=False)
+
         st = pd.Series(['ACT', 'NSW', 'NT', 'QLD', 'SA', 
                             'TAS', 'VIC', 'WA'])
         self.assertTrue(all(data[0]==st))
         
+
     def test_read_csv3(self):
+
         fcsv = '%s/multiindex.csv'%self.FOUT
+
         data, comment = csv.read_csv(fcsv)
 
         cols =['metric', 'runoff_rank',
@@ -35,6 +45,44 @@ class CsvTestCase(unittest.TestCase):
                 'yeojohnson-likelihood', 'yeojohnson-shapirotest']
 
         self.assertTrue(all(data.columns==cols))
+
+    def test_read_csv4(self):
+
+        fcsv = '%s/climate.csv.gz'%self.FOUT
+
+        data, comment = csv.read_csv(fcsv, 
+                parse_dates=[''], index_col=0)
+
+        self.assertTrue(len(comment) == 8)
+        self.assertTrue(comment['written_on'] == '2014-08-12 12:41')
+
+        d = data.index[0]
+        self.assertTrue(isinstance(d, pd.tslib.Timestamp))
+
+    def test_write_csv(self):
+
+        fcsv = '%s/testwrite.csv'%self.FOUT
+
+        nval = 100
+        nc = 5
+        idx = pd.date_range('1990-01-01', periods=nval, freq='D')
+        df1 = pd.DataFrame(np.random.normal(size=(nval, nc)), index=idx)
+
+        csv.write_csv(df1, fcsv, ['Random data'], 
+                os.path.abspath(__file__),
+                index=True)
+
+        df2, comment = csv.read_csv(fcsv, 
+                parse_dates=[''], index_col=0)
+
+        self.assertTrue(int(comment['nrow']) == nval)
+        self.assertTrue(int(comment['ncol']) == nc)
+
+        d = df2.index[0]
+        self.assertTrue(isinstance(d, pd.tslib.Timestamp))
+
+        self.assertTrue(np.allclose(df1, df2))
+
 
 if __name__ == "__main__":
     unittest.main()
