f86e2783c284bbce63b4abf47a60d854ac491246
Julien Lerat <julien.lerat@gmail.com>
2015-06-16 21:41:00 +1000

    fao query - new api - work in progress

--- hydrodiy/hydata/hyfao.py
+++ hydrodiy/hydata/hyfao.py
@@ -10,39 +10,7 @@ class HttpFAOError(Exception):
         pass
 
 
-fao_url = 'http://data.fao.org'
-
-fao_auth_key = 'd30aebf0-ab2a-11e1-afa6-0800200c9a66'
-
-faostat_url1 = 'http://faostat3.fao.org/faostat-api/rest'
-
-faostat_url2 = 'http://faostat3.fao.org/wds/api'
-
-object_columns = {
-        'countries':['country_code', 'country_label'], 
-        'regions':['region_code', 'region_label'], 
-        'specialgroups':['specialgroup_code', 
-            'specialgroup_label'], 
-        'items':['item_code', 'item_label'],
-        'elements':['element_code', 'element_label'],
-        'itemsaggregated':['itemagg_code', 'itemagg_label']
-    }
-
-
-def _get_url(type):
-
-    url = fao_url
-
-    if type == 'statistics':
-        url = '%s/statistics' % url
-
-    if type == 'resources':
-        url = '%s/developers/api/resources' % url
-
-    if type == 'catalogue':
-        url = '%s/developers/api/resources' % url
-
-    return url
+fao_url = 'http://data.fao.org/developers/api/v1/en/resources'
 
 
 class HyFAO():
@@ -51,192 +19,82 @@ class HyFAO():
 
         self.current_url = ''
 
-        self.base_params = {
-                'authKey':fao_auth_key,
-                'version':'1.0'
-            }
+    def list_databases(self):
 
-    def get_databases(self):
-
-        url = '%s/database' % _get_url('resources')
+        url = '%s/databases.json' % fao_url
         
-        req = requests.get(url, params = self.base_params)
+        req = requests.get(url)
 
         js = req.json()
+
+        self.current_url = req.url
 
         db = pd.DataFrame([it for it in js['result']['list']['items']])
         
         return db
 
-    def get_resource(self, uid, format='json'):
 
-        url = '%s/ref/%s.%s' % (fao_url, format) 
-        
-        req = requests.get(url, params = self.base_params)
+    def list_datasets(self, database):
 
+        url = '%s/%s/datasets.json' % (fao_url, database)
 
-        if format == 'json':
+        params = {'fields':
+                    'mnemonic%2Clabel%40en%2Cdescription%40en%2Cviews'}
 
-            js = req.json()
-
-            res = pd.DataFrame([it for it in js['result']['list']['items']])
-
-        else:
-            res = 
-        
-        return res
-
-
-
-    def get_domain_codes(self):
-
-        # Run query
-        base_url = ('%s/groupsanddomains/faostat/E') % (
-                        faostat_url1)
-
-        req = requests.get(base_url)
-
-        js = req.json()
-
-        # Convert json object to dataframe
-        df = pd.DataFrame(js)
-        
-        df.columns = ['group_code', 'group_label',
-                    'domain_code', 'domain_label',
-                    'order']
-
-        return df
-
-
-    def get_object_codes(self, object_name, domain_code):
-
-        if not object_name in object_columns:
-            raise ValueError(('object_name(%s) not in ['
-                '%s]') % (object_name, 
-                    '/'.join(object_columns.keys())))
-
-        # Run query
-        base_url = ('%s/procedures/%s/faostat/%s/E') % (
-                        faostat_url1, 
-                        object_name, domain_code)
-
-        req = requests.get(base_url)
+        req = requests.get(url, params = params)
 
         self.current_url = req.url
 
-        # Convert json object to dataframe
         js = req.json()
 
-        df = pd.DataFrame(js)
-
-        cc = object_columns[object_name]
-
-        df.columns = cc + ['X%d' % (i+1) 
-                            for i in range(df.shape[1]-len(cc))]
-
-        df['object_name'] = object_name
-        df['domain_code'] = domain_code
-
-        # Remove useless columns
-        cc = [cn for cn in df.columns if re.search('^X', cn)]
-        df = df.drop(cc, axis=1)
-
-        return df
-
-    def search_object_codes(self, object_name, domain_code, 
-                pattern):
-
-        object_codes = self.get_object_codes(object_name, 
-                                    domain_code)
-
-        cc = [cn for cn in object_codes.columns 
-                    if re.search('_label', cn)]
+        ds = pd.DataFrame([it for it in js['result']['list']['items']])
         
-        labels = object_codes[cc].squeeze()
-
-        idx = [bool(re.search(pattern, x)) for x in labels]
-
-        return object_codes.loc[idx,:]
+        return ds
 
 
-    def get_data(self, 
-            domain_code,
-            item_codes,
-            element_codes,
-            area_codes,
-            years = range(2010, 2014)):
+    def dataset_info(self, database, dataset):
 
-        ## Query parameters
-        #params = {
-        #    'db':'faostat',
-        #    'select':'A.AreaCode[FAOST_CODE],'
-        #                'D.year[Year],D.value[Value],'
-        #                'A.AreaNameE[AreaName],'
-        #                'E.elementnamee[ElementName],'
-        #                'I.itemnamee[ItemName]',
-        #    'from':'data[D],element[E],item[I],area[A]',
-        #    'where': ('D.elementcode(%s),D.itemcode(%s),'
-        #            'D.domaincode(\'%s\'),'
-        #            'JOIN(D.elementcode:E.elementcode),'
-        #            'JOIN(D.itemcode:I.itemcode),'
-        #            'JOIN(D.areacode:A.areacode)') % (
-        #                    element_code, item_code, domain_code),
-        #    'orderby':'E.elementnamee,D.year',
-        #    'out':'json'
-        #}
+        url = '%s/%s/%s' % (fao_url, 
+                        database, dataset)
 
-        #if not countries is None:
-        #    params['where'] = '%s,A.AreaCode(%s)' % (params['where'], 
-        #                            ':'.join(countries))
+        self.current_url = url
 
-        payload = {
-                'datasource': 'faostat',
-                'domainCode': domain_code,
-                'lang' : 'E',
-                'areaCodes': area_codes,
-                'itemCodes' : item_codes,
-                'elementListCodes' : element_codes,
-                'years' : years,
-                'flags': True,
-                'codes': True,
-                'units': True,
-                'nullValues': False,
-                'thousandSeparator' : ',',
-                'decimalSeparator': '.',
-                'decimalPlaces': 2,
-                'limit':-1
-        }
+        params = {'fields': 'mnemonic%2Clabel%40en'}
 
-        # Request data
-        #headers = {'Content-type': 'application/json',
-        #            'Accept': 'text/plain'} 
-
-        url = '%s/procedures/data' % faostat_url1
-
-        req = requests.post(url, json = payload)
-                #data = json.dumps(payload),
-                #headers = headers)
-
-        print(req.status_code)
-        import pdb; pdb.set_trace()
-
-        if req.status_code != 200:
-            raise HttpFAOError('URL : %s\nRequest status : %d' % 
-                    (req.url, req.status_code))
-
-        self.current_url = req.url
-
-        # Convert json object to dataframe
+        req = requests.get('%s/dimensions.json?' % url, params = params)
         js = req.json()
+        dims = pd.DataFrame([it for it in js['result']['list']['items']])
+        
+        req = requests.get('%s/members.json?' % url, params = params)
+        js = req.json()
+        membs = pd.DataFrame([it for it in js['result']['list']['items']])
+        
+        req = requests.get('%s/cnt/members.json?' % url, params = params)
+        js = req.json()
+        countries = pd.DataFrame([it for it in js['result']['list']['items']])
+        
+        return dims, membs, countries
 
-        df = pd.DataFrame(js)
 
-        df.columns = list(df.iloc[0, :].values)
-        df = df.iloc[1:,:]
+    def data(self, database, dataset, country=None, year=None):
 
-        df['domain_code'] = domain_code
-        df['element_code'] = element_code
-        df['item_code'] = item_code
+        url = '%s/%s/%s' % (fao_url, 
+                        database, dataset)
 
-        return df
-    
+        params = {'fields': 'mnemonic%2Clabel%40en'}
+
+        req = requests.get('%s/dimensions.json?' % url0, params = params)
+        js = req.json()
+        dims = pd.DataFrame([it for it in js['result']['list']['items']])
+        
+        req = requests.get('%s/members.json?' % url0, params = params)
+        js = req.json()
+        membs = pd.DataFrame([it for it in js['result']['list']['items']])
+        
+        req = requests.get('%s/cnt/members.json?' % url0, params = params)
+        js = req.json()
+        countries = pd.DataFrame([it for it in js['result']['list']['items']])
+        
+        return dims, membs, countries
+
+
--- hydrodiy/hydata/tests/test_hydata_hyfao.py
+++ hydrodiy/hydata/tests/test_hydata_hyfao.py
@@ -12,75 +12,29 @@ class HyFAOTestCase(unittest.TestCase):
         print('\t=> HyFaoTestCase (hydata)')
         self.dt = None # TODO
 
-    def test_get_databases(self):
+    def test_list_databases(self):
        
         hyf = hyfao.HyFAO()
 
-        db = hyf.get_databases()
+        db = hyf.list_databases()
 
-        import pdb; pdb.set_trace()
+        self.assertTrue('FAOSTAT' in list(db['label']))
 
-        self.assertTrue(dcodes.shape == (67, 5))
 
-#    def test_get_domain_codes(self):
-#       
-#        hyf = hyfao.HyFAO()
-#
-#        dcodes = hyf.get_domain_codes()
-#
-#        self.assertTrue(dcodes.shape == (67, 5))
-#
-#
-#    def test_get_object_codes(self):
-#       
-#        hyf = hyfao.HyFAO()
-#
-#        for o in hyfao.object_columns:
-#            ocodes = hyf.get_object_codes(o, 'GE')
-#
-#
-#    def test_search_object_codes(self):
-#       
-#        hyf = hyfao.HyFAO()
-#    
-#        domain_code = 'QC'
-#
-#        items = hyf.search_object_codes('items', 
-#                    domain_code, '(R|r)ice')
-#
-#        self.assertTrue(items.shape == (1, 4))
-#
-#
-#    def test_get_data(self):
-#       
-#        hyf = hyfao.HyFAO()
-#    
-#        domain_code = 'QC'
-#
-#        items = hyf.search_object_codes('items', 
-#                    domain_code, '(R|r)ice').squeeze()
-#
-#        item_codes = [items['item_code']]
-#        item_codes = ['800', '221', '711', '515', '526']
-#
-#
-#        elements = hyf.search_object_codes('elements', 
-#                        domain_code, '(P|p)roduction').squeeze()
-#        
-#        element_codes = [elements['element_code']]
-#        element_codes = ['2312', '2413', '2510']
-#
-#        area_codes = ['5000']
-#        area_codes = ['1', '4', '5']
-#
-#        data = hyf.get_data(
-#            domain_code = domain_code,
-#            item_codes = item_codes,
-#            element_codes = element_codes,
-#            area_codes = area_codes,
-#            years = ['2012', '2011', '2010'])
-#
-#        import pdb; pdb.set_trace()
+    def test_list_datasets(self):
+       
+        hyf = hyfao.HyFAO()
+
+        ds = hyf.list_datasets(database='faostat')
+
+        self.assertTrue('crop-prod' in list(ds['mnemonic']))
+
+
+    def test_dataset_info(self):
+       
+        hyf = hyfao.HyFAO()
+
+        info = hyf.dataset_info(database='faostat', dataset='crop-prod')
 
 
 if __name__ == "__main__":
