Added oz coast + fixed seasonal computation in dutils

hydrodiy/hydata/dutils.py
@@ -317,7 +317,7 @@ def runclimcum(data, clim, wateryear_startmonth, nwin=20):
 
     return climc, datat
 
-def to_seasonal(ts, nmonths=3):
+def to_seasonal(ts, nmonths=3, ngapmax=6):
     ''' Convert time series to seasonal time step
 
     Parameters
@@ -355,7 +355,23 @@ def to_seasonal(ts, nmonths=3):
     '''
     
     # Resample to monthly
-    tsm = ts.resample('MS', 'sum')
+    tsmm = ts.groupby(ts.index.month).mean()
+
+    def _sum(x):
+        ngap = np.sum(pd.isnull(x))
+
+        if ngap > ngapmax:
+            return np.nan
+
+        elif ngap == 0:
+            return x.sum()
+
+        else:
+            mv = tsmm[x.index.month[0]]
+            xx = x.fillna(mv)
+            return xx.sum()
+
+    tsm = ts.resample('MS', how=_sum)
 
     # Shift series
     tss = []

hydrodiy/hydata/tests/test_hydata_dutils.py
@@ -142,9 +142,13 @@ class UtilsTestCase(unittest.TestCase):
        idx = np.random.choice(range(len(u)), len(u)/50)
        u.iloc[idx] = np.nan
 
-       out = dutils.to_seasonal(u)
+       out = dutils.to_seasonal(u, ngapmax=0)
 
-       um = u.resample('MS','sum')
+       def _sum(x):
+            return np.sum(x.values)
+
+       um = u.resample('MS', how=_sum)
+
        expected = um+um.shift(-1)+um.shift(-2)
 
        idx1 = pd.notnull(expected)