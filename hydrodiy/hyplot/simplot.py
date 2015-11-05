1aa5ec9c54be4d87d4411ac0be77e6c41a9177b5
Julien Lerat <julien.lerat@gmail.com>
2015-11-05 19:22:00 +1100

    Added simplot

--- hydrodiy/hyplot/simplot.py
+++ hydrodiy/hyplot/simplot.py
@@ -1,7 +1,14 @@
 
+from datetime import datetime
+
+import pandas as pd
+import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+from hydata import dutils
+from hyplot import putils
 
 
 class Simplot(object):
@@ -10,37 +17,167 @@ class Simplot(object):
         fig, \
         obs, \
         sim, \
-        nfloods=4):
+        wateryear_start =7, \
+        nfloods=4, \
+        nbeforepeak = 30, \
+        nafterpeak = 60):
 
-        # Figure to draw on
-        self.fig = fig
+        # Properties
+        self.wateryear_start = wateryear_start
+
+        # data
+        self.idx = pd.notnull(obs) & (obs >= 0)
+        
+        if obs.name is None:
+            obs = pd.Series(obs, name='obs')
+        self.data = pd.DataFrame(obs)
+        
+        self.nsim = 1
+        if sim.name is None:
+            sim = pd.Series(sim, name='sim {0}'.format(self.nsim))
+
+        self.data = self.data.join(sim)
 
         # Number of flood events
         self.nfloods = nfloods
+        self.nbeforepeak = nbeforepeak
+        self.nafterpeak = nafterpeak
+        self._get_flood_indexes()
+
+        # Figure to draw on
+        self.fig = fig
 
         # Grid spec
         fig_ncols = 1 + nfloods/2
         fig_nrows = 3
         self.gs = gridspec.GridSpec(fig_nrows, fig_ncols,
                 width_ratios=[1] * fig_ncols,
-                height_ratios=[1] * fig_nrows)
+                height_ratios=[1] * (fig_nrows-1) + [0.5] * 1)
 
-        # data
-        self.idx = pd.notnull(obs) & (obs >= 0)
-        self.data = pd.DataFrame(obs)
+
+    def _get_flood_indexes(self):
+        
+        obs_tmp = self.data['obs'].copy()
+        nval = len(obs_tmp)
+        self.flood_idx = []
+
+        for iflood in range(self.nfloods):
+            imax = np.where(obs_tmp == obs_tmp.max())[0]
+            i1 = max(0, imax - self.nbeforepeak)
+            i2 = min(nval-1, imax + self.nafterpeak)
+            idx = range(i1, i2+1)
+            self.flood_idx.append(idx)
+
+            obs_tmp.iloc[idx] = np.nan
+
+
+    def add_sim(self, sim):
+
+        self.nsim += 1
+        if sim.name is None:
+            sim = pd.Series(sim, name='sim {0}'.format(self.nsim))
+
         self.data = self.data.join(sim)
 
-    def draw_fdc(self):
-        pass
 
-    def draw_floods(self):
-        pass
+    def draw(self):
 
-    def draw_annual(self):
-        pass
+        # Draw flow duration curves
+        ax = plt.subplot(self.gs[0, 0])
+        self.draw_fdc(ax)
 
-    def draw_balance(self):
-        pass
+        ax = plt.subplot(self.gs[1, 0])
+        self.draw_fdc(ax, xlog=True, ylog=False)
+
+        # Draw flood events
+        for iflood in range(self.nfloods):
+            ix = iflood/2 % self.nfloods 
+            iy = 1 + iflood % (self.nfloods/2)
+            ax = plt.subplot(self.gs[ix, iy])
+            self.draw_floods(ax, iflood)
+
+        # Draw annual time series
+        ax = plt.subplot(self.gs[2, 1:])
+        self.draw_annual(ax)
+
+        # Draw water balance
+        ax = plt.subplot(self.gs[2, 0])
+        self.draw_balance(ax)
+
+
+    
+    def draw_fdc(self, ax, xlog=False, ylog=True):
+        
+        if xlog:
+            ax.set_xscale('log', nonposx='clip')
+
+        if ylog:
+            ax.set_yscale('log', nonposx='clip')
+
+        # Freqency
+        idx = self.idx
+        nval = np.sum(idx)
+        ff = (np.arange(1, nval+1)-0.3)/(nval+0.4)
+        data = self.data
+
+        for cn in data.columns:
+            value = np.sort(data.loc[idx, cn].values)[::-1]
+            ax.plot(ff, value, label=cn)
+        
+        ax.set_xlabel('Frequency')
+        ax.set_ylabel('Flow')
+        ax.set_title('Flow duration curve')
+        ax.legend(loc=1, frameon=False)
+        ax.grid()
+
+
+    def draw_floods(self, ax, iflood):
+
+        data = self.data
+        idx = self.flood_idx[iflood]
+        dates = data.index.values[idx]
+
+        data.loc[dates, :].plot(ax=ax)
+
+        values = data.loc[dates, 'obs'].values
+        yy = dates[values == np.max(values)][0]
+        title = 'Flood #{0} - {1:%Y-%m}'.format(iflood+1, pd.to_datetime(yy))
+        ax.set_title(title)
+        ax.grid()
+
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Flow')
+
+
+    def draw_annual(self, ax):
+
+        # Compute annual time series
+        datam = self.data.apply(dutils.aggmonths, args=(1,))
+        datay = datam.apply(pd.rolling_sum, args=(12,))
+
+        im = self.wateryear_start - 1
+        if im == 0:
+            im = 12
+        datay = datay.loc[datay.index.month == im,:]
+        datay = datay.shift(-1)
+
+        # plot
+        datay.plot(ax=ax)
+
+        ax.set_ylabel('Annual flow')
+        ax.grid()
+
+
+    def draw_balance(self, ax):
+        data = self.data
+        datab = data.loc[self.idx, :].mean()
+
+        # plot
+        datab.plot(ax=ax, kind='bar')
+
+        ax.set_ylabel('Average flow')
+        ax.set_title('Water Balance')
+        ax.grid()
 
 
 
--- hydrodiy/hyplot/tests/test_hyplot_simplot.py
+++ hydrodiy/hyplot/tests/test_hyplot_simplot.py
@@ -5,63 +5,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from hyplot import putils
+from hyplot.simplot import Simplot
 from hygis import oz
 
-class UtilsTestCase(unittest.TestCase):
+class SimplotTestCase(unittest.TestCase):
 
     def setUp(self):
-        print('\t=> UtilsTestCase (hyplot)')
+        print('\t=> SimplotTestCase (hyplot)')
         FTEST, testfile = os.path.split(__file__)
         self.FOUT = FTEST
-        
-    def test_img2html(self):
-        lb = ['MDB', 'Gulf', 'North East Coast']
-        url1 = ['urn:bom.gov.au:awris:common:codelist:feature:%s'%id \
-                for id in ['dartmouth', 'julius', 'lakedalrymple']] 
-        url2 = ['urn:bom.gov.au:awris:common:codelist:feature:%s'%id \
-                    for id in ['wyangala', '', 'lakemaraboon']] 
-        root_http = 'http://water.bom.gov.au/waterstorage/resources/chart'
-        df = pd.DataFrame({'rowlabel':lb, 'url1':url1, 'url2':url2})
-        fo = '%s/storage1.html'%self.FOUT
-        html = putils.img2html('Storage data online', df, root_http, fo)
 
-        df = pd.DataFrame({'url1':url1, 'url2':url2})
-        fo = '%s/storage2.html'%self.FOUT
-        html = putils.img2html('Storage data online', df, root_http, fo)
+    def test_sim(self):
 
+        plt.close('all')
+        fig = plt.figure()
 
-    def test_col2cmap(self):
+        dt = pd.date_range('2010-01-01', '2015-12-01')
+        nval = len(dt)
 
-        colors = {1:'#004C99', 0:'#FF9933', 0.3:'#FF99FF'}
+        obs = pd.Series(np.exp(np.random.normal(size=nval)), index=dt)
+        sim = pd.Series(np.exp(np.random.normal(size=nval)), index=dt)
 
-        cmap = putils.col2cmap(colors)
+        sm = Simplot(fig, obs, sim)
+        sm.draw()
 
-        x = np.arange(1, 257).reshape((1,256))
-        fig, ax = plt.subplots()
-
-        ax.pcolor(x, cmap=cmap, vmin=1, vmax=256)
-
-        fp = '%s/cmap.png' % self.FOUT
-        fig.savefig(fp)
-
-
-    def test_line(self):
-
-        fig, ax = plt.subplots()
-
-        nval = 100
-        x = np.random.normal(size=nval)
-        y = np.random.normal(scale=2, size=nval)
-
-        ax.plot(x, y)
-
-        putils.line(ax, 1, 1, '-')
-        putils.line(ax, 1, 0, '--')
-        putils.line(ax, 1, np.inf, ':')
-        putils.line(ax, 2, np.inf, '-.')
-
-        fp = '%s/line.png' % self.FOUT
+        fp = '%s/simplot.png' % self.FOUT
+        fig.set_size_inches((15, 15))
+        sm.gs.tight_layout(fig)
         fig.savefig(fp)
 
 
