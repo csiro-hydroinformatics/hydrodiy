f0e4786440e44706be29ec3f4d49978ad1fee54f
Julien Lerat <julien.lerat@gmail.com>
2016-04-04 19:58:11 +1000

    added lognorm shifted censored distribution

--- hydrodiy/stat/distributions.py
+++ hydrodiy/stat/distributions.py
@@ -6,58 +6,68 @@ from scipy.stats import rv_continuous, norm
 
 from scipy.optimize import fmin_powell as fmin
 
-class LogNormCensored(rv_continuous):
-    ''' Log-Normal distribution with left censoring '''
+
+class LogNormShiftedCensored(rv_continuous):
+    ''' Log-Normal distribution with left censoring and shifting '''
 
     def __init__(self, censor=0.):
+
+        if not np.isfinite(censor):
+            raise ValueError('Censor ({0}) should be finite'.format(censor))
+
         rv_continuous.__init__(self,
-            name='lognormcensored',
-            longname='Left censored log-normal distribution',
+            name='lognormscensored',
+            longname='Left censored log-normal distribution with shifting parameter',
+            shapes='mu,sig,shift',
             a=censor)
 
-    def __checkparams(self, shift, censor):
+
+    def _argcheck(self, *args):
+        cond = 1
+        for arg in args:
+            cond = np.logical_and(cond, (np.isfinite(np.asarray(arg))))
+        return cond
+
+
+    def _pdf(self, x, mu, sig, shift):
+        censor = self.a
         if shift <= -censor:
             raise ValueError('shift({0}) <= -censor ({1})'.format(shift,
             -censor))
 
-    def _pdf(self, x, mu, sig, shift):
-        censor = self.a
-        self.__checkparams(shift, censor)
         pp = norm.pdf((np.log(x+shift)-mu)/sig)/(x+shift)
-        pp[np.allclose(x, censor)] = np.inf
+        pp[np.isclose(x, censor)] = np.inf
         pp[x<censor] = 0.
-        return y
+        return pp
+
 
     def _cdf(self, x, mu, sig, shift):
         censor = self.a
-        self.__checkparams(shift, censor)
+        if shift <= -censor:
+            raise ValueError('shift({0}) <= -censor ({1})'.format(shift,
+            -censor))
+
         cp = norm.cdf((np.log(x+shift)-mu)/sig)
+        import pdb;pdb.set_trace()
         cp[x<censor] = 0.
-        return y
+        return cp
 
     def _ppf(self, q, mu, sig, shift):
         censor = self.a
-        self.__checkparams(shift, censor)
+        if shift <= -censor:
+            raise ValueError('shift({0}) <= -censor ({1})'.format(shift,
+            -censor))
+
         x = np.exp(sig*norm.ppf(q)+mu)-a
         x[x<censor] = censor
         return x
 
-    def _stat(self, mu, sig, shift):
-
-        # TODO = Fix this
-        mean = 0.
-        stdev = 0.
-        skew = 0.
-        kurt = 0.
-
-        return mean, stdev, skew, kurt
-
     def _fitstart(self, data):
         data = np.asarray(data)
-        lm = np.log(data)
+        lm = np.log(data[data>0.])
         mu = np.nanmean(lm)
-        sig = math.sqrt(np.nanmean((lm-mu)**2))
-        return mu, sig, 1e-10
+        sig = math.sqrt(np.mean((lm-mu)**2))
+        return mu, sig, np.nan
 
     @inherit_docstring_from(rv_continuous)
     def fit(self, data, *args, **kwargs):
@@ -79,33 +89,28 @@ class LogNormCensored(rv_continuous):
             logsig = params[1]
             sig = math.exp(params[1])
 
-            if not np.isinf(censor):
-                shift = -censor + math.exp(params[2])
-            else:
-                shift = math.sinh(params[2])
-
-            # compute likelihood
-            xs = shift+data[idx1]
-            lxs = np.log(xs)
-            err = (lxs-mu)/sig
-            ll = (nval-n0)*logsig + np.sum(lxs+err*err/2)
+            shift = -censor + math.exp(params[2])
+            lx = np.log(shift+data[idx1])
+            err = (lx-mu)/sig
+            ll = (nval-n0)*logsig + np.sum(lx+err*err/2)
 
             P0 = norm.cdf((math.log(shift+censor)-mu)/sig)
             if P0 > 0:
                 ll -= n0*math.log(P0)
 
+            if np.isnan(ll):
+                raise ValueError('Nan value returned by likelihood')
+
             return ll
 
         # Start parameters
-        mu, sig, shift = self._fitstart(data)
-        if not np.isinf(censor):
-            params0 = [mu, math.log(sig), math.log(shift+censor)]
-        else:
-            params0 = [mu, math.log(sig), math.asinh(shift+censor)]
+        mu, sig, _ = self._fitstart(data)
+        params0 = [mu, math.log(sig), math.log(1e-30+censor)]
 
         # Optimization
         params, fopt, direc, niter, feval, _ = fmin(loglikelihood,
-            params0, maxfun=1000, full_output=True, disp=False)
+            params0, maxfun=1000, xtol=1e-5, ftol=1e-5,
+            full_output=True, disp=False)
 
         mu = params[0]
         sig = math.exp(params[1])
@@ -113,4 +118,6 @@ class LogNormCensored(rv_continuous):
 
         return (mu, sig, shift, 0., 1.)
 
+
+lognormscensored0 = LogNormShiftedCensored(censor=0.)
 
--- hydrodiy/stat/tests/test_hystat_distributions.py
+++ hydrodiy/stat/tests/test_hystat_distributions.py
@@ -6,12 +6,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from hydrodiy.stat.distributions import LogNormCensored
+from hydrodiy.stat.distributions import lognormscensored0
+
+from scipy.stats import _distn_infrastructure as infr
+from scipy.special import erf
 
 class LogNormCensoredTestCase(unittest.TestCase):
 
     def setUp(self):
-        print('\n\t=> LogNormCensoredTestCase (hystat)')
+        print('\n\t=> LogNormShiftedCensoredTestCase (hystat)')
         FTEST = os.path.dirname(os.path.abspath(__file__))
         self.FOUT = FTEST
 
@@ -19,20 +22,46 @@ class LogNormCensoredTestCase(unittest.TestCase):
         mu = 0.
         sig = 1.
         shift = 0.5
-        lnc = LogNormCensored()
-        lnc.pdf(2., mu, sig, shift)
+        x = 2.
+
+        res = lognormscensored0.pdf(x, mu, sig, shift)
+        expected = math.exp(-(math.log(x+shift)-mu)**2/2/sig**2)
+        expected = expected/(x+shift)/sig/math.sqrt(2*math.pi)
+        self.assertTrue(np.allclose(res, expected))
+
+        res = lognormscensored0.pdf(0., mu, sig, shift)
+        self.assertTrue(np.allclose(res, np.inf))
+
+        res = lognormscensored0.pdf(-1, mu, sig, shift)
+        self.assertTrue(np.allclose(res, 0.))
+
+    def test_cdf(self):
+        mu = 0.
+        sig = 1.
+        shift = 0.5
+        x = 2.
+
+        res = lognormscensored0.cdf(x, mu, sig, shift)
+        expected = 0.5*(1+erf((math.log(x+shift)-mu)/math.sqrt(2)/sig))
+        self.assertTrue(np.allclose(res, expected))
+
+        res = lognormscensored0.cdf(0, mu, sig, shift)
+        expected = 0.5*(1+erf((math.log(shift)-mu)/math.sqrt(2)/sig))
+        self.assertTrue(np.allclose(res, expected))
+
+        res = lognormscensored0.cdf(-1, mu, sig, shift)
+        self.assertTrue(np.allclose(res, 0.))
 
 
     def test_fit(self):
         nval = 5000
-        mu = 1.
+        mu = -1.
         sig = 2.
-        shift = -0.9
-        censor = 1.
+        shift = 0.1
         x = np.exp(np.random.normal(mu, sig, size=nval))-shift
-        x[x<censor] = censor
+        x[x<0.] = 0.
 
-        lnc = LogNormCensored(censor)
-        params = lnc.fit(x)
-        ck = np.allclose(params[:3], (mu, sig, shift), rtol=0.05)
+        params = lognormscensored0.fit(x)
+        ck = np.allclose(params[:3], (mu, sig, shift), rtol=0.1)
+        self.assertTrue(ck)
 
