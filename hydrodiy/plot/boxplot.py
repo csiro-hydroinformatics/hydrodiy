3b10bcb62abc516e3ea135eac12b161bcafacdac
Julien Lerat <julien.lerat@gmail.com>
2016-09-28 20:16:00 +1000

    Added more props to boxplot

--- hydrodiy/plot/boxplot.py
+++ hydrodiy/plot/boxplot.py
@@ -11,7 +11,7 @@ COLORS = putils.tableau_colors
 PROPS_NAMES = ['median', 'whisker', 'box', 'count', 'minmax']
 
 PROPS_VALUES = ['linestyle', 'linewidth', 'linecolor', 'va', 'ha', 'fontcolor', \
-    'textformat', 'fontsize', 'marker', 'markercolor']
+    'textformat', 'fontsize', 'marker', 'markercolor', 'showline', 'showtext']
 
 
 def perct(data):
@@ -26,15 +26,15 @@ def perct(data):
 class Boxplot(object):
 
     def __init__(self, data, ax=None, by=None, default_width=0.7, \
-                width_from_number=False, show_values=True, \
-                show_minmax=False):
+                width_from_number=False):
         ''' Draw boxplots with labels and defined colors '''
 
         if not by is None:
-            if not isinstance(by, pd.Series):
-                raise ValueError('by is not a Series')
-            if not isinstance(data, pd.Series):
-                raise ValueError('data is not a Series')
+            by = pd.Series(by)
+            data = pd.Series(data)
+        else:
+            data = pd.DataFrame(data)
+
         self._data = data
         self._by = by
 
@@ -47,20 +47,20 @@ class Boxplot(object):
 
         self._width_from_number = width_from_number
 
-        self._show_values = show_values
-        self._show_minmax = show_minmax
-
         self._props = {
             'median':{'linestyle':'-', 'linewidth':3, 'linecolor':COLORS[3], \
                 'va':'center', 'ha':'left', 'fontcolor':COLORS[3], \
-                'textformat':'%0.1f', 'fontsize':9}, \
-            'whisker':{'linestyle':'-', 'linewidth':2, 'linecolor':COLORS[0]}, \
-            'minmax':{'marker':'o', 'markercolor':COLORS[0]}, \
+                'textformat':'%0.1f', 'fontsize':9, 'showline':True, \
+                'showtext':True}, \
+            'whisker':{'linestyle':'-', 'linewidth':2, 'linecolor':COLORS[0], \
+                            'showline':True}, \
+            'minmax':{'marker':'o', 'markercolor':COLORS[0], 'showline':False}, \
             'box':{'linestyle':'-', 'linewidth':3, 'linecolor':COLORS[0], \
                 'va':'center', 'ha':'left', 'fontcolor':COLORS[0], \
-                'textformat':'%0.1f', 'fontsize':8}, \
+                'textformat':'%0.1f', 'fontsize':8, 'showline':True, \
+                'showtext':True}, \
             'count':{'fontcolor':'grey', 'textformat':'%d', \
-                'fontsize':7},
+                'fontsize':7, 'showtext':True},
         }
 
         self._compute()
@@ -69,11 +69,11 @@ class Boxplot(object):
     def __setitem__(self, key, value):
         ''' Set boxplot properties '''
         if not key in PROPS_NAMES:
-            raise ValueError('Cannot set properties for '+key)
+            raise ValueError('Cannot set property '+key)
 
         for k in value:
             if not k in PROPS_VALUES:
-                raise ValueError('Cannot set values '+k)
+                raise ValueError('Cannot set value '+k)
             self._props[key][k] = value[k]
 
 
@@ -98,9 +98,6 @@ class Boxplot(object):
         ax = self._ax
         stats = self._stats
         default_width = self._default_width
-        show_values = self._show_values
-        show_minmax = self._show_minmax
-
         ncols = stats.shape[1]
 
         # Boxplot widths
@@ -119,10 +116,11 @@ class Boxplot(object):
             y = [med] * 2
             props = self._props['median']
 
-            ax.plot(x, y, lw=props['linewidth'],
-                color=props['linecolor'])
+            if props['showline']:
+                ax.plot(x, y, lw=props['linewidth'],
+                    color=props['linecolor'])
 
-            if show_values:
+            if props['showtext']:
                 formatter = props['textformat']
                 if props['ha'] == 'left':
                     formatter = ' '+formatter
@@ -138,32 +136,37 @@ class Boxplot(object):
             y =  [q1, q1, q2, q2 ,q1]
             props = self._props['box']
 
-            ax.plot(x, y, lw=props['linewidth'],
-                color=props['linecolor'])
+            if props['showline']:
+                ax.plot(x, y, lw=props['linewidth'],
+                    color=props['linecolor'])
 
             # Draw whiskers and caps
             props = self._props['whisker']
 
-            for cc in [['10.0%', '25.0%'], ['90.0%', '75.0%']]:
-                q1 = stats.loc[cc[0], cn]
-                q2 = stats.loc[cc[1], cn]
-                x = [i]*2
-                y = [q1, q2]
+            if props['showline']:
+                for cc in [['10.0%', '25.0%'], ['90.0%', '75.0%']]:
+                    q1 = stats.loc[cc[0], cn]
+                    q2 = stats.loc[cc[1], cn]
+                    x = [i]*2
+                    y = [q1, q2]
 
-                ax.plot(x, y, lw=props['linewidth'],
-                    color=props['linecolor'])
+                    ax.plot(x, y, lw=props['linewidth'],
+                        color=props['linecolor'])
 
-                x = [i-w/5, i+w/5]
-                y = [q1]*2
-                ax.plot(x, y, lw=props['linewidth'],
-                    color=props['linecolor'])
+                    x = [i-w/5, i+w/5]
+                    y = [q1]*2
+                    ax.plot(x, y, lw=props['linewidth'],
+                        color=props['linecolor'])
+
 
             # quartile values
             props = self._props['box']
-            if show_values:
+            if props['showtext']:
+
                 formatter = props['textformat']
                 if props['ha'] == 'left':
                     formatter = ' '+formatter
+
                 for value in stats.loc[['25.0%', '75.0%'], cn]:
                     valuetext = formatter % value
                     ax.text(i+w/2, value, valuetext, fontsize=props['fontsize'], \
@@ -171,17 +174,15 @@ class Boxplot(object):
                         va=props['va'], ha=props['ha'])
 
             # min / max
-            if show_minmax:
+            props = self._props['minmax']
+            if props['showline']:
                 x = [i]*2
                 y = stats.loc[['min', 'max'], cn]
-                props = self._props['minmax']
                 ax.plot(x, y, props['marker'], color=props['markercolor'])
 
         # X tick labels
         ax.set_xticks(range(ncols))
         ax.set_xticklabels(stats.columns)
-
-        ax.set_xlim((-default_width, ncols-1+default_width))
 
         if not self._by is None:
             ax.set_xlabel(self._by.name)
@@ -190,9 +191,18 @@ class Boxplot(object):
         if logscale:
             ax.set_yscale('log', nonposy='clip')
 
+        # X/Y limits
+        ax.set_xlim((-default_width, ncols-1+default_width))
+
+        ylim = ax.get_ylim()
+        dy = ylim[1]-ylim[0]
+        ylim0 = min(ylim[0], stats.loc['10.0%', :].min()-dy*0.02)
+        ylim1 = max(ylim[1], stats.loc['90.0%', :].max()+dy*0.02)
+        ax.set_ylim((ylim0, ylim1))
+
         # Display count
-        if show_values:
-            props = self._props['count']
+        props = self._props['count']
+        if props['showtext']:
             formatter = '('+props['textformat']+')'
 
             ylim = ax.get_ylim()
--- hydrodiy/plot/tests/test_hyplot_boxplot.py
+++ hydrodiy/plot/tests/test_hyplot_boxplot.py
@@ -42,8 +42,21 @@ class BoxplotTestCase(unittest.TestCase):
         bx = Boxplot(ax=ax, data=self.data)
         bx['median'] = {'linecolor':'green'}
         bx['box'] = {'linewidth':5}
+        bx['minmax'] = {'showline':True, 'marker':'*'}
+
+        try:
+            bx['med'] = {'linecolor':'green'}
+        except ValueError as err:
+            self.assertTrue(str(err).startswith('Cannot set property'))
+
+        try:
+            bx['median'] = {'bidule':3}
+        except ValueError as err:
+            self.assertTrue(str(err).startswith('Cannot set value'))
+
         bx.draw()
         fig.savefig(os.path.join(self.FOUT, 'bx3.png'))
+
 
     def test_by(self):
         fig, ax = plt.subplots()
@@ -52,11 +65,21 @@ class BoxplotTestCase(unittest.TestCase):
         fig.savefig(os.path.join(self.FOUT, 'bx4.png'))
 
 
-    def test_draw_minmax(self):
+    def test_numpy(self):
         fig, ax = plt.subplots()
-        bx = Boxplot(ax=ax, data=self.data, show_minmax=True)
+        data = np.random.uniform(0, 10, size=(1000, 6))
+        bx = Boxplot(ax=ax, data=data)
         bx.draw()
         fig.savefig(os.path.join(self.FOUT, 'bx5.png'))
+
+
+    def test_log(self):
+        fig, ax = plt.subplots()
+        bx = Boxplot(ax=ax, data=self.data+3)
+        bx.draw(logscale=True)
+        fig.savefig(os.path.join(self.FOUT, 'bx6.png'))
+
+
 
 if __name__ == "__main__":
     unittest.main()
