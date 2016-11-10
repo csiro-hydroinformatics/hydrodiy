360030908bbcb3c7f3c972a24d6166b38434195e
Julien Lerat <julien.lerat@gmail.com>
2016-11-10 23:50:18 +1100

    Removed img2html

--- hydrodiy/plot/tests/test_hyplot_putils.py
+++ hydrodiy/plot/tests/test_hyplot_putils.py
@@ -15,33 +15,14 @@ class UtilsTestCase(unittest.TestCase):
         FTEST, testfile = os.path.split(__file__)
         self.FOUT = FTEST
 
-    def test_img2html(self):
-        lb = ['MDB', 'Gulf', 'North East Coast']
-        url1 = ['urn:bom.gov.au:awris:common:codelist:feature:%s'%id \
-                for id in ['dartmouth', 'julius', 'lakedalrymple']]
-        url2 = ['urn:bom.gov.au:awris:common:codelist:feature:%s'%id \
-                    for id in ['wyangala', '', 'lakemaraboon']]
-        root_http = 'http://water.bom.gov.au/waterstorage/resources/chart'
-        df = pd.DataFrame({'rowlabel':lb, 'url1':url1, 'url2':url2})
-        fo = '%s/storage1.html'%self.FOUT
-        html = putils.img2html('Storage data online', df, root_http, fo)
-
-        df = pd.DataFrame({'url1':url1, 'url2':url2})
-        fo = '%s/storage2.html'%self.FOUT
-        html = putils.img2html('Storage data online', df, root_http, fo)
-
-
     def test_col2cmap(self):
 
         colors = {1:'#004C99', 0:'#FF9933', 0.3:'#FF99FF'}
-
         cmap = putils.col2cmap(colors)
 
         x = np.arange(1, 257).reshape((1,256))
         fig, ax = plt.subplots()
-
         ax.pcolor(x, cmap=cmap, vmin=1, vmax=256)
-
         fp = '%s/cmap.png' % self.FOUT
         fig.savefig(fp)
 
