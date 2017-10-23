6cbbc9b0565e5df9074bed993196c46f55b0dca1
Julien Lerat <julien.lerat@gmail.com>
2017-10-23 19:02:13 +1100

    Added hykiwis functionality

---
+++ hydrodiy/data/hykiwis.py
@@ -0,0 +1,204 @@
+import re
+import requests
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+from hydrodiy.data.qualitycontrol import islinear
+
+#
+# This code is largely pasted from the kiwis_py package
+# developed by Andrew McDonald, Bureau of Meteorology, EHP
+#
+
+KIWIS_URL_EXT = 'http://www.bom.gov.au/waterdata/KiWIS/KiWIS'
+KIWIS_URL_INT ='http://ccfvp-wadiapp04:8080/KiWIS/KiWIS'
+
+# Base parameters for Kiwis server request
+BASE_PARAMS = {\
+    'service':'kisters', \
+    'type':'queryServices', \
+    'datasource':0, \
+    'format':'json' \
+}
+
+# Kiwis time series names
+TS_NAMES = {\
+    'as_stored': 'DMQaQc.Merged.AsStored.1', \
+    'daily_9am': 'DMQaQc.Merged.DailyMean.09HR', \
+    'daily_9am_qa': 'PR01QaQc.Merged.DailyMean.09HR'
+}
+
+# Default start year for data download
+START_YEAR = 1950
+
+
+def __testjson(req):
+    ''' Test validity of json conversion '''
+
+    try:
+        out = req.json()
+        return out
+
+    except ValueError as err:
+        warnings.warn('Formatting of json data return {0}'.format(err))
+        return None
+
+
+def get_sites(external=False):
+    ''' Get list of BoM Kiwis stations
+
+    Parameters
+    -----------
+    external : bool
+        Use Bureau of Meterology external Kiwis server. If False, use Bureau internal
+        server (accessible within Bureau network only).
+
+    Returns
+    -----------
+    sites : pandas.core.frame.DataFrame
+        List of BoM Kiwis sites
+    url : str
+        Url used to query the Kiwis server
+    '''
+
+    # Download site list
+    params = dict(BASE_PARAMS)
+    params.update({
+                'request': 'getStationList',
+                'returnfields':'station_no,station_id,'
+                    'station_latitude,station_longitude'
+    })
+    url = KIWIS_URL_EXT if external else KIWIS_URL_INT
+    req = requests.get(url, params=params)
+
+    # Format list of sites
+    sites = __testjson(req)
+    if sites is None or re.search('No matches', ' '.join(sites[0])):
+        raise ValueError('No data')
+
+    sites = pd.DataFrame(sites[1:], columns = sites[0])
+
+    return sites, url
+
+
+def get_tsattrs(siteid, ts_name='daily_9am_qa', external=True):
+    ''' Retrieve time series ID from site ID
+
+    Parameters
+    -----------
+    siteid : str
+        Site ID in Kiwis server
+    ts_name : str
+        Name of the Kiwis time series. Options are:
+        * daily_9am : Daily aggregated series from 9.00 to 9.00.
+                        Timestamping at the end of the time step.
+        * daily_9am_qa : Same than daily_9am with quality control
+        * as_stored : Series as provided by the data provider (e.g. streamflow
+                                data are continuous)
+    external : bool
+        Use Bureau of Meterology external Kiwis server. If False, use Bureau internal
+        server (accessible within Bureau network only).
+
+    Returns
+    -----------
+    tsattrs : dict
+        Kiwis attributes
+    url : str
+        Url used to query the Kiwis server
+    '''
+
+    # Check inputs
+    if not ts_name in TS_NAMES:
+        raise ValueError('Expected ts_name in {0}, got {1}'.format(\
+            list(TS_NAMES.keys()), ts_name))
+
+    # Download attributes
+    params = dict(BASE_PARAMS)
+    params.update({
+            'request': 'getTimeseriesList',
+            'ts_name': TS_NAMES[ts_name],
+            'station_no':siteid,
+            'returnfields':'ts_id,ts_unitname,ts_unitsymbol,'
+                'station_no,station_name,coverage'
+        })
+
+    url = KIWIS_URL_EXT if external else KIWIS_URL_INT
+    req = requests.get(url, params=params)
+
+    tsattrs = __testjson(req)
+    if tsattrs is None or re.search('No matches', ''.join(tsattrs[0])):
+        raise ValueError('No data')
+
+    # Format attributes
+    tsattrs = {k:v for k, v in zip(tsattrs[0], tsattrs[1])}
+    tsattrs['ts_name'] = ts_name
+
+    return tsattrs, url
+
+
+def get_data(tsattrs, start=None, end=None, external=True, keep_timezone=False):
+    ''' Download Kiwis data
+
+    Parameters
+    -----------
+    tsattrs : dict
+        Kiwis attributes of the timeseries. See hydrodiy.hykiwis.get_tsattrs
+    start : str
+        Start date of the requested time series including time zone. If None,
+        uses the 'from' field from tsattrs.
+    end : str
+        End date of the requested time series including time zone. If None,
+        uses the 'to' field from tsattrs.
+    external : bool
+        Use Bureau of Meterology external Kiwis server. If False, use Bureau internal
+        server (accessible within Bureau network only).
+    keep_timezone : bool
+        Keep time zone information in date parsing
+
+    Returns
+    -----------
+    ts : pandas.core.series.Series
+        Time series
+    url : str
+        Url used to query the Kiwis server
+    '''
+    # Download data
+    if start is None:
+        start = '{0}-01-01'.format(START_YEAR) + tsattrs['from'][10:]
+
+    if end is None:
+        end = tsattrs['to']
+
+    params = {'request':'getTimeseriesValues',
+        'ts_id':tsattrs['ts_id'],
+        'from': start,
+        'to': end
+    }
+    params.update(BASE_PARAMS)
+    params['format'] = 'dajson'
+
+    url = KIWIS_URL_EXT if external else KIWIS_URL_INT
+    req = requests.get(url, params=params)
+
+    # Convert to pandas series
+    js = __testjson(req)
+
+    if js is None or len(js[0]['data']) == 0:
+        ts = pd.Series([])
+
+    else:
+        d = pd.DataFrame(js[0]['data'], columns=['time', 'value'])
+
+        # Discard time zone
+        if keep_timezone:
+            time = pd.to_datetime(d['time'])
+        else:
+            time = pd.to_datetime(d['time'].apply(lambda x: \
+                                    re.sub('\+.*', '', x)))
+
+        ts = d.set_index(time)['value']
+        ts.name = '%s[%s]' % (tsattrs['ts_id'], tsattrs['ts_unitsymbol'])
+
+    return ts, url
+
---
+++ hydrodiy/data/tests/test_hydata_hykiwis.py
@@ -0,0 +1,63 @@
+import os
+import unittest
+import numpy as np
+from datetime import datetime
+import pandas as pd
+
+from hydrodiy.data import hykiwis
+
+class HyKiwisTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print('\t=> HyKiwisTestCase (hydata)')
+
+        source_file = os.path.abspath(__file__)
+        self.ftest = os.path.dirname(source_file)
+
+    def test_getsites(self):
+        ''' Test get sites '''
+
+        sites, url = hykiwis.get_sites()
+        self.assertTrue(not sites is None)
+        self.assertTrue(isinstance(sites, pd.core.frame.DataFrame))
+
+
+    def test_getattrs(self):
+        ''' Test get attributes '''
+
+        attrs, url = hykiwis.get_tsattrs('410001')
+        self.assertTrue(isinstance(attrs, dict))
+        self.assertEqual(attrs['station_name'], 'M/BIDGEE R @ WAGGA')
+        self.assertEqual(attrs['ts_unitsymbol'], 'Ml/d')
+        self.assertEqual(attrs['station_no'], '410001')
+
+        attrs, url = hykiwis.get_tsattrs('613002')
+        self.assertEqual(attrs['station_name'], 'DINGO ROAD')
+        self.assertEqual(attrs['ts_unitsymbol'], 'Ml/d')
+        self.assertEqual(attrs['station_no'], '613002')
+
+
+    def test_getdata(self):
+        ''' Test download data '''
+
+        # Full download
+        attrs, url = hykiwis.get_tsattrs('410001')
+        ts_data1, url = hykiwis.get_data(attrs)
+        self.assertTrue(isinstance(ts_data1, pd.core.series.Series))
+        self.assertEqual(ts_data1.index[0].year, hykiwis.START_YEAR)
+
+        # Restricted download
+        start = '2001-01-01'+attrs['to'][10:]
+        end = '2001-01-05'+attrs['to'][10:]
+        ts_data2, url = hykiwis.get_data(attrs, start, end)
+
+        index = ts_data2.index + pd.tseries.offsets.DateOffset(hours=-9)
+        expected = pd.date_range('2001-01-01', '2001-01-05')
+
+        v1 = index.values.astype(float)
+        v2 = expected.values.astype(float)
+        self.assertTrue(np.allclose(v1, v2))
+
+
+if __name__ == "__main__":
+    unittest.main()
