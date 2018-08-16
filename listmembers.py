3e7aa8ff093a7548739443e46109f9a8aa86006e
Julien Lerat <julien.lerat@gmail.com>
2018-08-16 19:11:35 +1000

    Added a script to list module classes/functions

---
+++ listmembers.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, re
+import pandas as pd
+import importlib
+from inspect import getmembers, isfunction, isclass, getsourcefile, \
+                        isbuiltin, getdoc
+from hydrodiy.io import iutils, csv
+
+#----------------------------------------------------------------------
+# Config
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+# Folders
+#----------------------------------------------------------------------
+source_file = os.path.abspath(__file__)
+froot = os.path.dirname(source_file)
+fhydrodiy = os.path.join(froot, 'hydrodiy')
+
+#----------------------------------------------------------------------
+# Get data
+#----------------------------------------------------------------------
+lf = iutils.find_files(fhydrodiy, '(?<=[a-z])\.py$')
+
+#----------------------------------------------------------------------
+# Process
+#----------------------------------------------------------------------
+
+members = pd.DataFrame(columns=['package', 'module', \
+                'object_name', 'type', 'doc'])
+
+for f in lf:
+    if re.search('version|template|test', f):
+        continue
+
+    name = re.sub('.py$', '', os.path.basename(f))
+    modname = re.split(os.path.sep, os.path.dirname(f))[-1]
+    import_name = 'hydrodiy.{0}.{1}'.format(modname, name)
+    module = importlib.import_module(import_name, package='hydrodiy')
+
+    for obj in getmembers(module):
+
+        # Check object is a class or a function
+        if isfunction(obj[1]) or isclass(obj[1]):
+
+            # Check object resides in hydrodiy
+            skip = False
+            try:
+                if not re.search('hydrodiy', getsourcefile(obj[1])):
+                    skip = True
+            except TypeError:
+                skip = True
+
+            if skip:
+                continue
+
+            # Get doc
+            try:
+                doc = re.sub('\\n.*', '', getdoc(obj[1]))
+            except TypeError:
+                doc = ''
+
+            # Store data
+            tp = 'class' if isclass(obj[1]) else 'function'
+            dd = {\
+                    'package': 'hydrodiy',\
+                    'module': import_name,\
+                    'object_name': obj[0], \
+                    'type': tp, \
+                    'doc': doc
+            }
+            members = members.append(dd, ignore_index=True)
+
+
+comment = 'List of classes and functions in hydrodiy'
+fc = os.path.join(froot, 'listmembers.html')
+pd.set_option('display.max_colwidth', 200)
+members.to_html(fc)
+#csv.write_csv(members, fc, comment, source_file, compress=False)
+
+
