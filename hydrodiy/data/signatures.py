fixed fdcslope computation

hydrodiy/data/signatures.py
@@ -124,8 +124,8 @@ def fdcslope(x, q1=90, q2=100, cst=0.375):
         return np.nan, qq
 
     # Compute slope
-    M = np.column_stack([np.ones(nqq), xr[idx]])
-    theta, _ , _, _ = np.linalg.lstsq(M, ff[idx])
+    M = np.column_stack([np.ones(nqq), ff[idx]])
+    theta, _ , _, _ = np.linalg.lstsq(M, xr[idx])
 
     return theta[1], qq
 

hydrodiy/data/tests/test_hydata_signatures.py
@@ -36,7 +36,7 @@ class SignaturesTestCase(unittest.TestCase):
         ''' Test fdc slope computation '''
         x = np.linspace(0, 1, 101)
         slp, qq = signatures.fdcslope(x, q1=90, q2=100, cst=0.5)
-        self.assertTrue(np.isclose(slp, 0.99009901))
+        self.assertTrue(np.isclose(slp, 1.01))
         self.assertTrue(np.allclose(qq, [0.9, 1]))
 
 