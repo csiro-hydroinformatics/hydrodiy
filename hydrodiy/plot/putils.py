b47ce9b48bbfe8e0b030776dc51dbf715454cc56
Julien Lerat <julien.lerat@gmail.com>
2019-05-05 23:12:34 +1000

    Added doc to putils.ecdfplot

--- hydrodiy/plot/putils.py
+++ hydrodiy/plot/putils.py
@@ -695,6 +695,11 @@ def ecdfplot(ax, df, label_stat=None, label_stat_format='4.2f', \
     args, kwargs
         Argument sent to matplotlib.pyplot.plot command for each
 
+    Returns
+    -----------
+    lines : dict
+        Dictionnary containing the line object for each column
+        in df
     '''
     lines = {}
     for name, se in df.iteritems():
