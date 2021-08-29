9eb545a8e2e788d009065eed245058d56e2a7955
Julien Lerat <julien.lerat@gmail.com>
2021-08-29 22:13:28 +1000

    chore: Cleaned setup script.[WIP]

--- setup.py
+++ setup.py
@@ -18,59 +18,60 @@ def read(fname):
 
 
 # Define Cython C extensions
-if os.getenv('HYDRODIY_NO_BUILD') == '1':
+if os.getenv("HYDRODIY_NO_BUILD") == "1":
     # Not extension
     ext_modules = []
 else:
     ext_modules = [
         Extension(
-            name='c_hydrodiy_data',
+            name="c_hydrodiy_data",
             sources=[
-                'hydrodiy/data/c_hydrodiy_data.pyx',
-                'hydrodiy/data/c_dateutils.c',
-                'hydrodiy/data/c_qualitycontrol.c',
-                'hydrodiy/data/c_dutils.c',
-                'hydrodiy/data/c_var2h.c',
-                'hydrodiy/data/c_baseflow.c'
+                "hydrodiy/data/c_hydrodiy_data.pyx",
+                "hydrodiy/data/c_dateutils.c",
+                "hydrodiy/data/c_qualitycontrol.c",
+                "hydrodiy/data/c_dutils.c",
+                "hydrodiy/data/c_var2h.c",
+                "hydrodiy/data/c_baseflow.c"
             ],
             include_dirs=[numpy.get_include()]),
 
         Extension(
-            name='c_hydrodiy_stat',
+            name="c_hydrodiy_stat",
             sources=[
-                'hydrodiy/stat/c_hydrodiy_stat.pyx',
-                'hydrodiy/stat/c_crps.c',
-                'hydrodiy/stat/c_dscore.c',
-                'hydrodiy/stat/c_olsleverage.c',
-                'hydrodiy/stat/c_armodels.c',
-                'hydrodiy/stat/ADinf.c',
-                'hydrodiy/stat/AnDarl.c',
-                'hydrodiy/stat/c_andersondarling.c',
-                'hydrodiy/stat/c_paretofront.c'
+                "hydrodiy/stat/c_hydrodiy_stat.pyx",
+                "hydrodiy/stat/c_crps.c",
+                "hydrodiy/stat/c_dscore.c",
+                "hydrodiy/stat/c_olsleverage.c",
+                "hydrodiy/stat/c_armodels.c",
+                "hydrodiy/stat/ADinf.c",
+                "hydrodiy/stat/AnDarl.c",
+                "hydrodiy/stat/c_andersondarling.c",
+                "hydrodiy/stat/c_paretofront.c"
             ],
             include_dirs=[numpy.get_include()]),
         Extension(
-            name='c_hydrodiy_gis',
+            name="c_hydrodiy_gis",
             sources=[
-                'hydrodiy/gis/c_hydrodiy_gis.pyx',
-                'hydrodiy/gis/c_grid.c',
-                'hydrodiy/gis/c_catchment.c',
-                'hydrodiy/gis/c_points_inside_polygon.c'
+                "hydrodiy/gis/c_hydrodiy_gis.pyx",
+                "hydrodiy/gis/c_grid.c",
+                "hydrodiy/gis/c_catchment.c",
+                "hydrodiy/gis/c_points_inside_polygon.c"
             ],
             include_dirs=[numpy.get_include()])
     ]
 
 cmdclass = versioneer.get_cmdclass()
-cmdclass['build_ext'] = build_ext
+cmdclass["build_ext"] = build_ext
 
 # Setup config
 setup(
-    name = 'hydrodiy',
+    name = "hydrodiy",
+    author = "Julien Lerat",
+    author_email = "julien.lerat@gmail.com",
+    url = "https://bitbucket.org/jlerat/hydrodiy",
+    download_url = "https://bitbucket.org/jlerat/hydrodiy/downloads",
     version= versioneer.get_version(),
-    description = 'Python tools to support hydrological modelling and data analysis',
-    author = 'Julien Lerat',
-    author_email = 'julien.lerat@gmail.com',
-    url = 'https://bitbucket.org/jlerat/hydrodiy',
+    description = "Python tools to support hydrological modelling and data analysis",
     packages = find_packages(),
     package_data = {
         "hydrodiy": ["gis/data/*.gz", \
@@ -97,19 +98,19 @@ setup(
     ],
     cmdclass = cmdclass,
     ext_modules = ext_modules,
-    test_suite='nose.collector',
+    test_suite="nose.collector",
     tests_require=[
-        'nose',
-        'pyproj'
+        "nose",
+        "pyproj"
     ],
     classifiers=[
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: BSD License'
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License"
     ]
 )
 
