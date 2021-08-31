086474af375099c69bd27aa9c6246588ad3e10dd
Julien Lerat <julien.lerat@gmail.com>
2021-08-31 23:45:45 +1000

    fix: Fixedimportlib in __init__.

--- hydrodiy/__init__.py
+++ hydrodiy/__init__.py
@@ -1,5 +1,5 @@
 import sys
-import imp
+import importlib
 
 # Detect python version
 PYVERSION = 2
@@ -7,11 +7,12 @@ if sys.version_info > (3, 0):
     PYVERSION = 3
 
 def has_c_module(name, raise_error=True):
-    try:
-        name = f"c_hydrodiy_{name}"
-        fp, pathname, description = imp.find_module(name)
+    name = f"c_hydrodiy_{name}"
+    out = importlib.util.find_spec(name)
+
+    if not out is None:
         return True
-    except ImportError:
+    else:
         if raise_error:
             raise ImportError(f"C module c_hydrodiy_{name} is "+\
                 "not available, please run python setup.py build")
