d3f50c46af767d9144296099a3becba0d74c34ca
Julien Lerat <julien.lerat@gmail.com>
2021-09-18 12:25:29 +1000

    fix: write json to memory before zipping in zipjson

--- hydrodiy/io/tests/test_hyio_zipjson.py
+++ hydrodiy/io/tests/test_hyio_zipjson.py
@@ -9,56 +9,57 @@ from hydrodiy.io import zipjson
 class ZipjsonTestCase(unittest.TestCase):
 
     def setUp(self):
-        print('\t=> ZipjsonTestCase')
+        print("\t=> ZipjsonTestCase")
         self.source_file = os.path.abspath(__file__)
         self.ftest = os.path.dirname(self.source_file)
-        self.data = {'key1': 1, 'key2': 'this is a string', \
-                        'key3': [0.4, 32.2, 12.45]}
+        self.data = {"key1": 1, "key2": "this is a string", \
+                        "key3": [0.4, 32.2, 12.45]}
 
     def test_write_error(self):
-        ''' Test zipjson writer errors '''
-        filename = os.path.join(self.ftest, 'zipjson_test2.zip')
-        source_file = 'bidule'
-        comment = 'test'
+        """ Test zipjson writer errors """
+        filename = os.path.join(self.ftest, "zipjson_test2.zip")
+        source_file = "bidule"
+        comment = "test"
         try:
             zipjson.write_zipjson(self.data, filename, comment, \
                                 source_file, indent=4)
         except ValueError as err:
-            self.assertTrue(str(err).startswith('Source file'))
+            self.assertTrue(str(err).startswith("Source file"))
         else:
-            raise ValueError('Problem with error handling')
+            raise ValueError("Problem with error handling")
 
         try:
-            zipjson.write_zipjson(self.data, filename+'.bb', comment, \
+            zipjson.write_zipjson(self.data, filename+".bb", comment, \
                                 self.source_file, indent=4)
         except ValueError as err:
-            self.assertTrue(str(err).startswith('Expected filename'))
+            self.assertTrue(str(err).startswith(\
+                    "Expected file extension to be .zip"))
         else:
-            raise ValueError('Problem with error handling')
+            raise ValueError("Problem with error handling")
 
 
     def test_read_error(self):
-        ''' Test zipjson reader error '''
-        filename = os.path.join(self.ftest, 'zipjson_error.zip')
+        """ Test zipjson reader error """
+        filename = os.path.join(self.ftest, "zipjson_error.zip")
         try:
             data, meta = zipjson.read_zipjson(filename)
         except ValueError as err:
-            self.assertTrue(str(err).startswith('No data'))
+            self.assertTrue(str(err).startswith("No data"))
         else:
-            raise ValueError('Problem with error handling')
+            raise ValueError("Problem with error handling")
 
 
     def test_read(self):
-        ''' Test zipjson reader '''
-        filename = os.path.join(self.ftest, 'zipjson_test.zip')
+        """ Test zipjson reader """
+        filename = os.path.join(self.ftest, "zipjson_test.zip")
         data, meta = zipjson.read_zipjson(filename)
         self.assertEqual(data, self.data)
 
 
     def test_write(self):
-        ''' Test zipjson writer '''
-        filename = os.path.join(self.ftest, 'zipjson_test2.zip')
-        comment = 'test'
+        """ Test zipjson writer """
+        filename = os.path.join(self.ftest, "zipjson_test2.zip")
+        comment = "test"
         zipjson.write_zipjson(self.data, filename, comment, \
                                 self.source_file, indent=4)
         data, meta = zipjson.read_zipjson(filename)
--- hydrodiy/io/zipjson.py
+++ hydrodiy/io/zipjson.py
@@ -1,27 +1,21 @@
-''' Module to read and write csv data with comments '''
+""" Module to read and write csv data with comments """
 
 import sys, os, re, json
+from pathlib import Path
 from datetime import datetime
 import zipfile
 import warnings
 
 from hydrodiy import PYVERSION
 
-# Tailor string handling depending on python version
-if PYVERSION == 2:
-    from StringIO import StringIO
-    UNICODE = unicode
-
-elif PYVERSION == 3:
-    from io import StringIO
-    UNICODE = str
+from io import StringIO
 
 class ZipjsonWriteError(ValueError):
-    ''' Error raised by write_zipjson '''
+    """ Error raised by write_zipjson """
     pass
 
 class ZipjsonReadError(ValueError):
-    ''' Error raised by write_zipjson '''
+    """ Error raised by write_zipjson """
     pass
 
 
@@ -29,14 +23,15 @@ def write_zipjson(data, filename, \
             comment, source_file, \
             author=None, \
             compression=zipfile.ZIP_DEFLATED, \
+            encoding="utf-8", \
             **kwargs):
-    ''' write a python object to a compressed json file
+    """ write a python object to a compressed json file
 
     Parameters
     -----------
     data : object
         Serializable python object
-    filename : str
+    filename : pathlib.Path
         Path to file
     comment : str (or dict)
         Comments to be added to header
@@ -46,51 +41,50 @@ def write_zipjson(data, filename, \
         Data author (default is given by os.getlogin)
     compression : str
         Compression level
+    encoding : str
+        Encoding scheme.
     kwargs : dict
         Arguments passed to json.dump
-    '''
-    # Check file extension
-    if not filename.endswith('.zip'):
-        raise ZipjsonWriteError('Expected filename extension to be zip')
+    """
+    filename = Path(filename)
+    if not filename.suffix == ".zip":
+        raise ValueError("Expected file extension to be .zip, "+\
+                        f"got {filename.suffix}")
 
     # Create dict object
-    towrite = {'data': data}
+    towrite = {"data": data}
 
     # Add file meta data
     if author is None:
         try:
             author = os.getlogin()
         except Exception:
-            author = 'unknown'
+            author = "unknown"
 
-    towrite['author'] = author
-    towrite['comment'] = comment
-    towrite['filename'] = filename
+    towrite["author"] = author
+    towrite["comment"] = comment
+    towrite["filename"] = str(filename)
 
-    if not os.path.exists(source_file):
-        raise ZipjsonWriteError('Source file {0} does not exists'.format(\
-                            source_file))
+    source_file = Path(source_file)
+    if not source_file.exists():
+        raise ZipjsonWriteError(\
+                f"Source file {source_file} does not exists")
 
-    towrite['source_file'] = source_file
+    towrite["source_file"] = str(source_file)
 
-    time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-    towrite['time_generated'] = time
-
-    # Write to Json
-    filename_txt = re.sub('\.zip', '.json', filename)
-    with open(filename_txt, 'w') as fo:
-        json.dump(towrite, fo, **kwargs)
+    time = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+    towrite["time_generated"] = time
 
     # Zip the file
-    with zipfile.ZipFile(filename, 'w', compression=compression) as archive:
-        archive.write(filename_txt, arcname=os.path.basename(filename_txt))
+    with zipfile.ZipFile(str(filename), "w", \
+                compression=compression) as archive:
+        with archive.open(filename.name, "w") as fo:
+                    fo.write(json.dumps(towrite, **kwargs)\
+                                    .encode(encoding))
 
-    # Remove file
-    os.remove(filename_txt)
 
-
-def read_zipjson(filename, encoding='utf-8', **kwargs):
-    ''' Read a json zipped file
+def read_zipjson(filename, encoding="utf-8", **kwargs):
+    """ Read a json zipped file
 
     Parameters
     -----------
@@ -100,28 +94,29 @@ def read_zipjson(filename, encoding='utf-8', **kwargs):
         Charactert encoding
     kwargs : dict
         Arguments passed to json.load
-    '''
+    """
     data = {}
     nfiles = 0
-    with zipfile.ZipFile(filename, 'r') as archive:
+    filename = Path(filename)
+    with zipfile.ZipFile(str(filename), "r") as archive:
         namelist = archive.namelist()
         if len(namelist) > 1:
-            raise ValueError(('Expected one file in zip file {0}, '+\
-                        'got {1}').format(filename, len(namelist)))
+            raise ValueError(("Expected one file in zip file {0}, "+\
+                        "got {1}").format(filename, len(namelist)))
 
         fname = namelist[0]
-        uni = UNICODE(archive.read(fname), encoding=encoding)
+        uni = str(archive.read(fname), encoding=encoding)
         with StringIO(uni) as fobj:
             meta = json.load(fobj, **kwargs)
 
-    if not 'data' in meta:
-        raise  ValueError('No data in json file')
+    if not "data" in meta:
+        raise  ValueError("No data in json file")
     else:
-        data = meta['data']
+        data = meta["data"]
 
-    meta.pop('data')
+    meta.pop("data")
     if len(meta) == 0:
-        warnings.warn('No meta-data in json file')
+        warnings.warn("No meta-data in json file")
 
     return data, meta
 
