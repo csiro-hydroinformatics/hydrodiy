3f2410f0782599e391d3126bf0c819122df56ef8
Julien Lerat <julien.lerat@gmail.com>
2022-01-28 12:37:54 +1100

    fix: fixed listmember

--- listmembers.py
+++ listmembers.py
@@ -4,6 +4,7 @@
 import sys, os, re
 import argparse
 import pandas as pd
+from pathlib import Path
 import importlib
 from inspect import getmembers, isfunction, isclass, getsourcefile, \
                         isbuiltin, getdoc
@@ -13,59 +14,55 @@ from hydrodiy.io import iutils, csv
 # Config
 #----------------------------------------------------------------------
 parser = argparse.ArgumentParser(\
-    description='List functions available in hydrodiy package', \
+    description="List functions available in hydrodiy package", \
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('-s', '--skipbasemap', \
-                    help='Skip code importing basemap package', \
-                    action='store_true', default=False)
+parser.add_argument("-s", "--skipbasemap", \
+                    help="Skip code importing basemap package", \
+                    action="store_true", default=False)
 args = parser.parse_args()
 skipbasemap = args.skipbasemap
 
 #----------------------------------------------------------------------
 # Folders
 #----------------------------------------------------------------------
-source_file = os.path.abspath(__file__)
-froot = os.path.dirname(source_file)
-fhydrodiy = os.path.join(froot, 'hydrodiy')
+source_file = Path(__file__).resolve()
+froot = source_file.parent
+fhydrodiy = froot / "hydrodiy"
 
-basename = re.sub('\\.py.*', '', os.path.basename(source_file))
-LOGGER = iutils.get_logger(basename)
+LOGGER = iutils.get_logger(source_file.stem)
 
 #----------------------------------------------------------------------
 # Get data
 #----------------------------------------------------------------------
-lf = iutils.find_files(fhydrodiy, '(?<=[a-z])\.py$')
+lf = fhydrodiy.glob("**/*.py")
 
 #----------------------------------------------------------------------
 # Process
 #----------------------------------------------------------------------
 
-members = pd.DataFrame(columns=['package', 'module', \
-                'object_name', 'type', 'doc'])
+members = pd.DataFrame(columns=["package", "module", \
+                "object_name", "type", "doc"])
 
 for f in lf:
-
-    if re.search('version|template|test', f):
+    if re.search("version|template|test|init", str(f)):
         continue
 
-    LOGGER.info('Inspecting {0}'.format(os.path.basename(f)))
+    LOGGER.info(f"Inspecting {f.name}")
 
     # Skip if it imports basemap
     if skipbasemap:
-        with open(f, 'r') as fo:
+        with f.open("r") as fo:
             txt = fo.read()
 
-        if re.search('basemap', txt):
-            LOGGER.info(('file {0} imports basemap, '+\
-                            'skip it').format(os.path.basename(f)))
+        if re.search("basemap", txt):
+            LOGGER.info("file {f.name} imports basemap, skip.")
             continue
 
     # Import the file
-    name = re.sub('.py$', '', os.path.basename(f))
-    modname = re.sub('.*[^A-Za-z]', '', os.path.dirname(f))
-    import_name = 'hydrodiy.{0}.{1}'.format(modname, name)
-    module = importlib.import_module(import_name, package='hydrodiy')
+    modname = f.parts[-2]
+    import_name = f"hydrodiy.{modname}.{f.stem}"
+    module = importlib.import_module(import_name, package="hydrodiy")
 
     for obj in getmembers(module):
 
@@ -75,7 +72,7 @@ for f in lf:
             # Check object resides in hydrodiy
             skip = False
             try:
-                if not re.search('hydrodiy', getsourcefile(obj[1])):
+                if not re.search("hydrodiy", getsourcefile(obj[1])):
                     skip = True
             except TypeError:
                 skip = True
@@ -85,25 +82,25 @@ for f in lf:
 
             # Get doc
             try:
-                doc = re.sub('\\n.*', '', getdoc(obj[1]))
+                doc = re.sub("\\n.*", "", getdoc(obj[1]))
             except TypeError:
-                doc = ''
+                doc = ""
 
             # Store data
-            tp = 'class' if isclass(obj[1]) else 'function'
+            tp = "class" if isclass(obj[1]) else "function"
             dd = {\
-                    'package': 'hydrodiy',\
-                    'module': import_name,\
-                    'object_name': obj[0], \
-                    'type': tp, \
-                    'doc': doc
+                    "package": "hydrodiy",\
+                    "module": import_name,\
+                    "object_name": obj[0], \
+                    "type": tp, \
+                    "doc": doc
             }
             members = members.append(dd, ignore_index=True)
 
 
-comment = 'List of classes and functions in hydrodiy'
-fc = os.path.join(froot, 'listmembers.html')
-pd.set_option('display.max_colwidth', 200)
+comment = "List of classes and functions in hydrodiy"
+fc = os.path.join(froot, "listmembers.html")
+pd.set_option("display.max_colwidth", 200)
 members.to_html(fc)
 #csv.write_csv(members, fc, comment, source_file, compress=False)
 
