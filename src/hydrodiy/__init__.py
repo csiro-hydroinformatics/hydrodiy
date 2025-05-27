4168e881d50a6aeb1f388815c07f7eef0118c045
Julien Lerat <julien.lerat@csiro.au>
2025-05-28 09:01:15 +1000

    fix: fixed style

--- src/hydrodiy/__init__.py
+++ src/hydrodiy/__init__.py
@@ -8,16 +8,17 @@ PYVERSION = 2
 if sys.version_info > (3, 0):
     PYVERSION = 3
 
+
 def has_c_module(name, raise_error=True):
     name = f"c_hydrodiy_{name}"
     out = importlib.util.find_spec(name)
 
-    if not out is None:
+    if out is not None:
         return True
     else:
         if raise_error:
-            raise ImportError(f"C module {name} is "+\
-                "not available, please run python setup.py build")
+            raise ImportError(f"C module {name} is "
+                              + "not available, please "
+                              + "run python setup.py build")
         else:
             return False
-
