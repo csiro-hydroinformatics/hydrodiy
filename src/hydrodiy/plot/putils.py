f1395f042a0eb0d0b65e23e2c73bbb6d58a594bf
Julien Lerat <julien.lerat@csiro.au>
2026-07-09 15:13:11 +1000

    feat: added log transform to kde plots

--- src/hydrodiy/plot/putils.py
+++ src/hydrodiy/plot/putils.py
@@ -374,7 +374,8 @@ def set_mpl(color_theme="black",
             mpl.rc("savefig", transparent=True)
 
 
-def kde(xy, ngrid=50, eps=1e-10, buffer_proportion=0.05):
+def kde(xy, ngrid=50, eps=1e-10, buffer_proportion=0.05,
+        logx=False, logy=False):
     """ Interpolate a 2d pdf from a set of x/y data points using
     a Gaussian KDE. The outputs can be used to plot the pdf
     with something like matplotlib.Axes.contourf
@@ -391,6 +392,10 @@ def kde(xy, ngrid=50, eps=1e-10, buffer_proportion=0.05):
     buffer_proportion : float
         Buffer addded to extend the domain boundaries
         beyond the data limits.
+    logx : bool
+        Perform kde approximation in log space for x.
+    logy : bool
+        Perform kde approximation in log space for x.
 
     Returns
     -----------
@@ -406,6 +411,12 @@ def kde(xy, ngrid=50, eps=1e-10, buffer_proportion=0.05):
 
     iok = np.all(np.isfinite(xy), axis=1)
     xy = xy[iok]
+
+    if logx:
+        xy[:, 0] = np.log(xy[:, 0])
+
+    if logy:
+        xy[:, 1] = np.log(xy[:, 1])
 
     x0 = np.nanmin(xy[:, 0])
     x1 = np.nanmax(xy[:, 0])
@@ -427,6 +438,12 @@ def kde(xy, ngrid=50, eps=1e-10, buffer_proportion=0.05):
     kd = gaussian_kde(xy.T)
     zz = kd(np.vstack([xx.ravel(), yy.ravel()]))
     zz = zz.reshape(xx.shape)
+
+    if logx:
+        xx = np.exp(xx)
+
+    if logy:
+        yy = np.exp(yy)
 
     return xx, yy, zz
 
--- src/hydrodiy/plot/tests/test_hyplot_putils.py
+++ src/hydrodiy/plot/tests/test_hyplot_putils.py
@@ -192,6 +192,33 @@ def test_kde():
     fig.savefig(fp)
 
 
+@pytest.mark.parametrize("axis", ["x", "y", "both"])
+def test_kde_log(axis):
+    xy = np.random.multivariate_normal(
+        [1, 2], [[1, 0.9], [0.9, 1]],
+        size=1000)
+    xy = np.exp(xy)
+
+    logx = axis in ["x", "both"]
+    logy = axis in ["y", "both"]
+    xx, yy, zz = putils.kde(xy, logx=logx, logy=logy)
+
+    plt.close("all")
+    fig, ax = plt.subplots()
+    cont = ax.contourf(xx, yy, zz, cmap="Blues")
+    ax.contour(cont, colors="grey")
+    ax.plot(xy[:, 0], xy[:, 1], ".", alpha=0.2, mfc="grey", mec="none")
+
+    if axis in ["x", "both"]:
+        ax.set_xscale("log")
+
+    if axis in ["y", "both"]:
+        ax.set_yscale("log")
+
+    fp = FIMG / f"kde_log_{axis}.png"
+    fig.savefig(fp)
+
+
 def test_kde_missing():
     xy = np.random.multivariate_normal( \
         [1, 2], [[1, 0.9], [0.9, 1]], \
